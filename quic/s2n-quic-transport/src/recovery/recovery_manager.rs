a24ac4eb622dcd73bc583e7b49c0fc110bc94bac
Wesley Rosenblum <wesleyr@amazon.com>
2020-07-27 20:23:11 -0700

    Initial commit

---
+++ quic/s2n-quic-transport/src/recovery/recovery_manager.rs
@@ -0,0 +1,94 @@
+use crate::recovery::{SentPacketInfo, SentPackets};
+use core::{cmp::max, time::Duration};
+use s2n_quic_core::{
+    ack_set::AckSet,
+    packet::number::{PacketNumber, PacketNumberSpace},
+    recovery::RTTEstimator,
+    transport::error::TransportError,
+};
+
+pub struct RecoveryManager {
+    packet_space: PacketNumberSpace,
+    rtt_estimator: RTTEstimator,
+    // max_ack_delay:
+    // The maximum amount of time by which the receiver intends to delay acknowledgments for packets in the ApplicationData packet number space. The actual ack_delay in a received ACK frame may be larger due to late timers, reordering, or lost ACK frames.
+    // loss_detection_timer:
+    // Multi-modal timer used for loss detection.
+    // pto_count:
+    // The number of times a PTO has been sent without receiving an ack.
+    // time_of_last_ack_eliciting_packet[kPacketNumberSpace]:
+    // The time the most recent ack-eliciting packet was sent.
+    // largest_acked_packet[kPacketNumberSpace]:
+    // The largest packet number acknowledged in the packet number space so far.
+    largest_acked_packet: Option<PacketNumber>,
+    // loss_time[kPacketNumberSpace]:
+    // The time at which the next packet in that packet number space will be considered lost based on exceeding the reordering window in time.
+    // sent_packets[kPacketNumberSpace]:
+    // An association of packet numbers in a packet number space to information about them.
+    sent_packets: SentPackets,
+}
+
+impl RecoveryManager {
+    //OnDatagramReceived
+
+    //OnAckReceived(ack, pn_space)
+    pub fn on_ack_received<A: AckSet>(
+        &mut self,
+        ack_set: &A,
+        ack_delay: Duration,
+    ) -> Result<(), TransportError> {
+        if let Some(largest_acked_packet) = self.largest_acked_packet {
+            self.largest_acked_packet = Some(max(largest_acked_packet, ack_set.largest()));
+        } else {
+            self.largest_acked_packet = Some(ack_set.largest());
+        }
+
+        // detect_and_remove_acked_packets finds packets that are newly
+        // acknowledged and removes them from sent_packets.
+        let newly_acked_packets = self.detect_and_remove_acked_packets(ack_set);
+        // Nothing to do if there are no newly acked packets.
+        if newly_acked_packets.is_empty() {
+            return Ok(());
+        }
+
+        let largest_newly_acked = newly_acked_packets
+            .last()
+            .expect("there must be at least one newly acked packet at this point");
+
+        // If the largest acknowledged is newly acked and
+        // at least one ack-eliciting was newly acked, update the RTT.
+        if largest_newly_acked.0 == ack_set.largest() {
+            let latest_rtt = s2n_quic_platform::time::now() - largest_newly_acked.1.time_sent;
+            self.rtt_estimator
+                .update_rtt(ack_delay, latest_rtt, largest_newly_acked.0.space());
+        };
+
+        // Process ECN information if present.
+
+        Ok(())
+    }
+
+    //SetLossDetectionTimer
+
+    //OnLossDetectionTimeout
+
+    //DetectAndRemoveLostPackets
+
+    // Finds packets that are newly acknowledged and removes them from sent_packets.
+    fn detect_and_remove_acked_packets<A: AckSet>(
+        &mut self,
+        ack_set: &A,
+    ) -> Vec<(PacketNumber, SentPacketInfo)> {
+        let newly_acked_packets = self
+            .sent_packets
+            .range(ack_set.smallest()..=ack_set.largest())
+            .map(|(pn, spi)| (pn.clone(), spi.clone()))
+            .collect();
+
+        for &(packet_number, _sent_packet_info) in &newly_acked_packets {
+            self.sent_packets.remove(packet_number);
+        }
+
+        newly_acked_packets
+    }
+}
