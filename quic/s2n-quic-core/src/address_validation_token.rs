05bcb396acc4c7b0f8163e64ad5cc8c4b971a08c
Ryan Day <rynday@amazon.com>
2020-08-28 19:19:52 +0000

    WIP

--- quic/s2n-quic-core/src/address_validation_token.rs
+++ quic/s2n-quic-core/src/address_validation_token.rs
@@ -34,39 +34,39 @@ decoder_value!(
 
 pub trait AddressValidation {}
 
+/// Maximum size of an address validation token
+const MAX_ADDRESS_VALIDATION_TOKEN_LEN: usize = 512;
+
 //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#8.1.4
 //#   There is no need for a single well-defined format for the token
 //#   because the server that generates the token also consumes it.
 pub struct AddressValidationToken {
-    //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#8.1.1
+    bytes: [u8; MAX_ADDRESS_VALIDATION_TOKEN_LEN],
+    len: u16,
+}
+
+impl AddressValidationToken {
+
+    //= https://tools.ietf.org/id/draft-ietf-quic-transport-29.txt#8.1.1
     //#   A token sent in a NEW_TOKEN frames or a Retry packet MUST be
     //#   constructed in a way that allows the server to identify how it was
     //#   provided to a client.  These tokens are carried in the same field,
     //#   but require different handling from servers.
-    token_type: TokenType,
+    pub fn get_token_type(&self) -> TokenType {}
 
-    //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#8.1.4
-    //#   Tokens sent in Retry packets SHOULD include information that allows the
-    //#   server to verify that the source IP address and port in client
-    //#   packets remain constant.
-    // TODO This is based on the logic used for the server PreferredAddress. Try to find a better
-    // way to handle ipv4 OR ipv6 logic.
-    ipv4_peer_address: Option<SocketAddressV4>,
-    ipv6_peer_address: Option<SocketAddressV6>,
-
-    //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#21.2
+    //= https://tools.ietf.org/id/draft-ietf-quic-transport-29.txt#21.2
     //#   An attacker might be able to receive an address validation token
     //#   (Section 8) from a server and then release the IP address it used to
     //#   acquire that token.
     //#   Servers SHOULD provide mitigations for this attack by limiting the
     //#   usage and lifetime of address validation tokens
-    lifetime: u64,
+    pub fn get_key_id(&self) -> u8 {}
 
     //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#8.1.3
     //#   An address validation token MUST be difficult to guess.  Including a
     //#   large enough random value in the token would be sufficient, but this
     //#   depends on the server remembering the value it sends to clients.
-    nonce: [u8; 16],
+    pub fn get_nonce(&self) -> u8 {}
 
     //= https://tools.ietf.org/html/draft-ietf-quic-transport-29.txt#8.1.3
     //#   A token-based scheme allows the server to offload any state
@@ -76,7 +76,7 @@ pub struct AddressValidationToken {
     //#   protection, malicious clients could generate or guess values for
     //#   tokens that would be accepted by the server.  Only the server
     //#   requires access to the integrity protection key for tokens.
-    mac: [u8; 32],
+    pub fn get_hmac(&self) -> u8 {}
 }
 
 impl AddressValidationToken {
--- quic/s2n-quic/src/provider/retry_token.rs
+++ quic/s2n-quic/src/provider/retry_token.rs
@@ -1,7 +1,47 @@
 /// Provides retry token support for an endpoint
+use s2n_quic_core::{
+    connection::ConnectionId,
+    inet::SocketAddress,
+};
+use core::time::Duration;
+
+//= https://tools.ietf.org/id/draft-ietf-quic-transport-29.txt#8.1.1
+//#   A token sent in a NEW_TOKEN frames or a Retry packet MUST be
+//#   constructed in a way that allows the server to identify how it was
+//#   provided to a client.  These tokens are carried in the same field,
+//#   but require different handling from servers.
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    RetryToken,
+    NewToken,
+}
+
 pub trait Provider {
     type RetryToken: 'static + Send;
     type Error: core::fmt::Display;
+
+    /// Called when a token is needed for a NEW_TOKEN frame.
+    fn generate_new_token(
+        &mut self,
+        peer_address: &SocketAddress,
+        destination_connection_id: &ConnectionId,
+        source_connection_id: &ConnectionId,
+        output_buffer: &mut [u8]) -> (usize, Duration);
+
+    /// Called when a token is needed for a Retry Packet.
+    fn generate_retry_token(
+        &mut self,
+        peer_address: &SocketAddress,
+        destination_connection_id: &ConnectionId,
+        source_connection_id: &ConnectionId,
+        output_buffer: &mut [u8]) -> (usize, Duration);
+
+    /// Called to validate a token.
+    fn is_token_valid(&mut self, peer_address: &SocketAddress, destination_connection_id: &ConnectionId, source_connection_id: &ConnectionId, token: &[u8]) -> bool;
+
+    /// Called to return the hash of a token for de-duplication purposes
+    fn get_token_hash(&self, token: &[u8]) -> &[u8];
+}
 
     fn start(self) -> Result<Self::RetryToken, Self::Error>;
 }
@@ -22,5 +62,47 @@ pub mod default {
             // TODO
             Ok(())
         }
+
+impl Provider for Default {
+
+}
+
+impl_provider_utils!();
+
+#[cfg(test)]
+mod token_tests {
+    use super::*;
+    use s2n_codec::{DecoderBufferMut, EncoderBuffer};
+
+    #[test]
+    fn test_encoding() {
+        let peer_address = SocketAddressV4::new([127, 0, 0, 1], 80).into();
+        let dcid = ConnectionId::default();
+        let scid = ConnectionId::default();
+        let mut token_buffer = vec![0; MAX_ADDRESS_VALIDATION_TOKEN_LEN];
+
+        let nonce: [u8; 16] = [1; 16];
+        let mac: [u8; 32] = [2; 32];
+        let token = Default {
+            token_type: TokenType::NewToken,
+            ipv4_peer_address: Some(SocketAddressV4::new([127, 0, 0, 1], 80).into()),
+            ipv6_peer_address: None,
+            lifetime: 0,
+            nonce,
+            mac,
+        };
+
+        let mut encoder = EncoderBuffer::new(&mut b);
+        token.encode(&mut encoder);
+
+        let decoder = DecoderBufferMut::new(&mut b);
+        let (decoded_token, _) = decoder.decode::<AddressValidationToken>().unwrap();
+
+        assert_eq!(token.token_type, decoded_token.token_type);
+        assert_eq!(token.nonce, decoded_token.nonce);
+        assert_eq!(token.mac, decoded_token.mac);
+        assert_eq!(token.lifetime, decoded_token.lifetime);
+        assert_eq!(token.ipv4_peer_address, decoded_token.ipv4_peer_address);
+        assert_eq!(token.ipv6_peer_address, decoded_token.ipv6_peer_address);
     }
 }
