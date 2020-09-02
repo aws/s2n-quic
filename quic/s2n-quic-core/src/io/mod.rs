0d42d8be290f566bb25ca4cf13a3cebe8d8e0468
Cameron Bytheway <bythewc@amazon.com>
2020-09-02 19:34:28 +0000

    core: rename io::Pair to io::Duplex (#106)

--- quic/s2n-quic-core/src/io/mod.rs
+++ quic/s2n-quic-core/src/io/mod.rs
@@ -4,13 +4,18 @@ pub mod rx;
 pub mod tx;
 
 /// A pair of Rx and Tx IO implementations
+///
+/// From https://en.wikipedia.org/wiki/Duplex_(telecommunications):
+///
+/// > A duplex communication system is a point-to-point system composed of two or more
+/// > connected parties or devices that can communicate with one another in both directions.
 #[derive(Debug)]
-pub struct Pair<Rx, Tx> {
+pub struct Duplex<Rx, Tx> {
     pub rx: Rx,
     pub tx: Tx,
 }
 
-impl<'a, Rx: rx::Rx<'a>, Tx> rx::Rx<'a> for Pair<Rx, Tx> {
+impl<'a, Rx: rx::Rx<'a>, Tx> rx::Rx<'a> for Duplex<Rx, Tx> {
     type Queue = Rx::Queue;
     type Error = Rx::Error;
 
@@ -27,7 +32,7 @@ impl<'a, Rx: rx::Rx<'a>, Tx> rx::Rx<'a> for Pair<Rx, Tx> {
     }
 }
 
-impl<'a, Rx, Tx: tx::Tx<'a>> tx::Tx<'a> for Pair<Rx, Tx> {
+impl<'a, Rx, Tx: tx::Tx<'a>> tx::Tx<'a> for Duplex<Rx, Tx> {
     type Queue = Tx::Queue;
     type Error = Tx::Error;
 
--- quic/s2n-quic/src/provider/io.rs
+++ quic/s2n-quic/src/provider/io.rs
@@ -1,71 +1,35 @@
 use s2n_quic_core::io::{rx, tx};
-pub use s2n_quic_core::{inet, io::Pair};
-pub use s2n_quic_platform::default;
-use std::{
-    io,
-    net::{SocketAddr, ToSocketAddrs},
-};
+pub use s2n_quic_core::{inet, io::Duplex};
+use std::io;
 
 /// Provides IO support for an endpoint
 pub trait Provider
 where
-    for<'a> Self::Pair: rx::Rx<'a> + tx::Tx<'a>,
+    for<'a> Self::Rx: 'static + Send + rx::Rx<'a>,
+    for<'a> Self::Tx: 'static + Send + tx::Tx<'a>,
 {
-    type Pair;
+    type Rx;
+    type Tx;
+    type Error: core::fmt::Display;
 
-    fn finish(self) -> io::Result<Self::Pair>;
+    fn start(self) -> Result<Duplex<Self::Rx, Self::Tx>, Self::Error>;
 }
 
-impl<Rx, Tx> Provider for Pair<Rx, Tx>
+impl<Rx, Tx> Provider for Duplex<Rx, Tx>
 where
-    for<'a> Rx: rx::Rx<'a>,
-    for<'a> Tx: tx::Tx<'a>,
+    for<'a> Rx: 'static + Send + rx::Rx<'a>,
+    for<'a> Tx: 'static + Send + tx::Tx<'a>,
 {
-    type Pair = Self;
+    type Rx = Rx;
+    type Tx = Tx;
+    type Error = io::Error;
 
-    fn finish(self) -> io::Result<Self> {
+    fn start(self) -> io::Result<Self> {
         Ok(self)
     }
 }
 
-#[derive(Debug)]
-pub struct Default {
-    addresses: Vec<SocketAddr>,
-}
-
-impl Default {
-    fn new<A: ToSocketAddrs>(addr: A) -> io::Result<Self> {
-        let addresses = addr.to_socket_addrs()?.collect();
-        Ok(Self { addresses })
-    }
-}
-
-impl core::default::Default for Default {
-    fn default() -> Self {
-        Self {
-            addresses: ("0.0.0.0", 443u16).to_socket_addrs().unwrap().collect(),
-        }
-    }
-}
-
-impl Provider for Default {
-    type Pair = default::Pair;
-
-    fn finish(self) -> io::Result<Self::Pair> {
-        let mut builder = default::Socket::builder()?;
-        for addr in self.addresses {
-            builder = builder.with_address(addr)?;
-        }
-
-        let socket = builder.build()?;
-
-        let rx = default::Rx::new(default::Buffer::default(), socket.try_clone()?);
-        let tx = default::Tx::new(default::Buffer::default(), socket);
-
-        let socket = default::Pair { rx, tx };
-        Ok(socket)
-    }
-}
+pub use platform::Provider as Default;
 
 impl TryInto for u16 {
     type Error = io::Error;
@@ -99,3 +63,52 @@ impl_socket_addrs!(inet::SocketAddressV4);
 impl_socket_addrs!(inet::SocketAddressV6);
 
 impl_provider_utils!();
+
+pub mod platform {
+    pub use s2n_quic_platform::default::*;
+    use std::{
+        io,
+        net::{SocketAddr, ToSocketAddrs},
+    };
+
+    #[derive(Debug)]
+    pub struct Provider {
+        addresses: Vec<SocketAddr>,
+    }
+
+    impl Provider {
+        pub fn new<A: ToSocketAddrs>(addr: A) -> io::Result<Self> {
+            let addresses = addr.to_socket_addrs()?.collect();
+            Ok(Self { addresses })
+        }
+    }
+
+    impl Default for Provider {
+        fn default() -> Self {
+            Self {
+                addresses: ("0.0.0.0", 443u16).to_socket_addrs().unwrap().collect(),
+            }
+        }
+    }
+
+    impl super::Provider for Provider {
+        type Rx = Rx;
+        type Tx = Tx;
+        type Error = io::Error;
+
+        fn start(self) -> io::Result<Duplex> {
+            let mut builder = Socket::builder()?;
+            for addr in self.addresses {
+                builder = builder.with_address(addr)?;
+            }
+
+            let socket = builder.build()?;
+
+            let rx = Rx::new(Buffer::default(), socket.try_clone()?);
+            let tx = Tx::new(Buffer::default(), socket);
+
+            let socket = Duplex { rx, tx };
+            Ok(socket)
+        }
+    }
+}
