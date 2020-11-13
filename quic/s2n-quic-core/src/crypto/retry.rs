1c1437926ecca78f21cb897d475bc9f7b6a028ac
Ryan Day <rynday@amazon.com>
2020-11-13 14:05:35 +0000

    PR feedback

--- quic/s2n-quic-core/src/crypto/retry.rs
+++ quic/s2n-quic-core/src/crypto/retry.rs
@@ -1,7 +1,17 @@
-use crate::crypto::{CryptoError, HeaderCrypto, Key};
+use crate::crypto::CryptoError;
 use hex_literal::hex;
 
 pub type IntegrityTag = [u8; 16];
+
+/// Types for which are able to perform cryptography.
+///
+/// This marker trait ensures only 1-RTT-level keys
+/// are used with Short packets. Any key misuses are
+/// caught by the type system.
+pub trait RetryCrypto {
+    fn generate_tag(payload: &[u8]) -> IntegrityTag;
+    fn validate(payload: &[u8], tag: IntegrityTag) -> Result<(), CryptoError>;
+}
 
 //= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#5.8
 //# The Retry Integrity Tag is a 128-bit field that is computed as the
@@ -17,12 +27,12 @@ pub const SECRET_KEY_BYTES: [u8; 16] = hex!("ccce187ed09a09d05728155a6cb96be1");
 
 pub const NONCE_BYTES: [u8; 12] = hex!("e54930f97f2136f0530a8c1c");
 
-/// Types for which are able to perform cryptography.
-///
-/// This marker trait ensures only 1-RTT-level keys
-/// are used with Short packets. Any key misuses are
-/// caught by the type system.
-pub trait RetryCrypto: Key + HeaderCrypto {
-    fn generate_tag(payload: &[u8]) -> IntegrityTag;
-    fn validate(payload: &[u8], tag: IntegrityTag) -> Result<(), CryptoError>;
-}
+//= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#A.4
+//# This shows a Retry packet that might be sent in response to the
+//# Initial packet in Appendix A.2.  The integrity check includes the
+//# client-chosen connection ID value of 0x8394c8f03e515708, but that
+//# value is not included in the final Retry packet:
+pub const EXAMPLE_PSEUDO_RETRY_PACKET: [u8; 29] =
+    hex!("088394c8f03e515708 ffff000020 00 08f067a5502a4262b5 746f6b656e");
+
+pub const EXAMPLE_EXPECTED_TAG: [u8; 16] = hex!("59756519dd6cc85bd90e33a934d2ff85");
--- quic/s2n-quic-ring/src/retry.rs
+++ quic/s2n-quic-ring/src/retry.rs
@@ -1,39 +1,37 @@
+use core::convert::TryInto;
 use ring::aead;
 use s2n_quic_core::crypto::{
     retry::{IntegrityTag, NONCE_BYTES, SECRET_KEY_BYTES},
     CryptoError, RetryCrypto,
 };
 
-negotiated_crypto!(RingRetryCrypto);
-
 lazy_static::lazy_static! {
-/// Compute the Initial salt once, as the seed is constant
-static ref SECRET_KEY: aead::LessSafeKey = aead::LessSafeKey::new(
+    /// Compute the Initial salt once, as the seed is constant
+    static ref SECRET_KEY: aead::LessSafeKey = aead::LessSafeKey::new(
         aead::UnboundKey::new(&aead::AES_128_GCM, &SECRET_KEY_BYTES).unwrap(),
     );
 }
 
+#[derive(Debug)]
+pub struct RingRetryCrypto;
+
 impl RetryCrypto for RingRetryCrypto {
     fn generate_tag(pseudo_packet: &[u8]) -> IntegrityTag {
-        // TODO use the static nonce, no need to create this every time.
-        let nonce: aead::Nonce = aead::Nonce::assume_unique_for_key(NONCE_BYTES);
+        let nonce = aead::Nonce::assume_unique_for_key(NONCE_BYTES);
         let tag = SECRET_KEY
             .seal_in_place_separate_tag(nonce, aead::Aad::from(pseudo_packet), &mut [])
-            .unwrap();
+            .expect("in_out len is 0 and should always be less than the nonce max bytes");
 
-        let mut integrity_tag = [0; 16];
-        integrity_tag.copy_from_slice(tag.as_ref());
-        integrity_tag
+        tag.as_ref()
+            .try_into()
+            .expect("AES_128_GCM tag len should always be 128 bits")
     }
 
     fn validate(pseudo_packet: &[u8], tag: IntegrityTag) -> Result<(), CryptoError> {
-        let t = RingRetryCrypto::generate_tag(pseudo_packet);
+        let expected = RingRetryCrypto::generate_tag(pseudo_packet);
 
-        if ring::constant_time::verify_slices_are_equal(&t, &tag).is_ok() {
-            return Ok(());
-        }
-
-        Err(CryptoError::DECRYPT_ERROR)
+        ring::constant_time::verify_slices_are_equal(&expected, &tag)
+            .map_err(|_| CryptoError::DECRYPT_ERROR)
     }
 }
 
@@ -41,23 +39,15 @@ impl RetryCrypto for RingRetryCrypto {
 mod tests {
     use super::*;
     use hex_literal::hex;
+    use s2n_quic_core::crypto::{EXAMPLE_EXPECTED_TAG, EXAMPLE_PSEUDO_RETRY_PACKET};
 
     #[test]
     fn test_valid_tag() {
-        //= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#A.4
-        //# This shows a Retry packet that might be sent in response to the
-        //# Initial packet in Appendix A.2.  The integrity check includes the
-        //# client-chosen connection ID value of 0x8394c8f03e515708, but that
-        //# value is not included in the final Retry packet:
+        let invalid_tag: [u8; 16] = hex!("00112233445566778899aabbccddeeff");
 
-        // Pseudo packet manually created based the retry packet and initial packet from Appendix A.2
-        // TODO use the Retry / PseudoRetry encoder and decoder for this.
-        let pseudo_packet = hex!("088394c8f03e515708 ffff000020 00 08f067a5502a4262b5 746f6b656e");
-
-        let expected_tag = hex!("59756519dd6cc85bd90e33a934d2ff85");
-        let invalid_tag = hex!("00112233445566778899aabbccddeeff");
-
-        assert!(RingRetryCrypto::validate(&pseudo_packet, expected_tag).is_ok());
-        assert!(RingRetryCrypto::validate(&pseudo_packet, invalid_tag).is_err());
+        assert!(
+            RingRetryCrypto::validate(&EXAMPLE_PSEUDO_RETRY_PACKET, EXAMPLE_EXPECTED_TAG).is_ok()
+        );
+        assert!(RingRetryCrypto::validate(&EXAMPLE_PSEUDO_RETRY_PACKET, invalid_tag).is_err());
     }
 }
