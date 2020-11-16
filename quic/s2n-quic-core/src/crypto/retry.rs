f0c9fa5c23f059601112c59d51dbe71d9d438dfa
Ryan Day <rynday@amazon.com>
2020-11-16 16:46:05 -0500

    Add Retry packet decoder (#230)
    * Add Retry packet decoder and tests

--- quic/s2n-quic-core/src/crypto/retry.rs
+++ quic/s2n-quic-core/src/crypto/retry.rs
@@ -1,7 +1,8 @@
 use crate::crypto::CryptoError;
 use hex_literal::hex;
 
-pub type IntegrityTag = [u8; 16];
+pub const INTEGRITY_TAG_LEN: usize = 16;
+pub type IntegrityTag = [u8; INTEGRITY_TAG_LEN];
 
 pub trait RetryCrypto {
     fn generate_tag(payload: &[u8]) -> IntegrityTag;
@@ -22,12 +23,40 @@ pub const SECRET_KEY_BYTES: [u8; 16] = hex!("ccce187ed09a09d05728155a6cb96be1");
 
 pub const NONCE_BYTES: [u8; 12] = hex!("e54930f97f2136f0530a8c1c");
 
-//= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#A.4
-//# This shows a Retry packet that might be sent in response to the
-//# Initial packet in Appendix A.2.  The integrity check includes the
-//# client-chosen connection ID value of 0x8394c8f03e515708, but that
-//# value is not included in the final Retry packet:
-pub const EXAMPLE_PSEUDO_RETRY_PACKET: [u8; 29] =
-    hex!("088394c8f03e515708 ffff000020 00 08f067a5502a4262b5 746f6b656e");
+pub mod example {
+    use super::*;
 
-pub const EXAMPLE_EXPECTED_TAG: [u8; 16] = hex!("59756519dd6cc85bd90e33a934d2ff85");
+    //= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#A.4
+    //# This shows a Retry packet that might be sent in response to the
+    //# Initial packet in Appendix A.2.  The integrity check includes the
+    //# client-chosen connection ID value of 0x8394c8f03e515708, but that
+    //# value is not included in the final Retry packet:
+    //#
+    //# ffff0000200008f067a5502a4262b574 6f6b656e59756519dd6cc85bd90e33a9
+    //# 34d2ff85
+    pub const PACKET: [u8; 36] = hex!(
+        "
+    ffff0000200008f067a5502a4262b5 746f6b656e59756519dd6cc85bd90e33a9
+    34d2ff85
+    "
+    );
+
+    pub const PSEUDO_PACKET: [u8; 29] =
+        hex!("088394c8f03e515708 ffff000020 00 08f067a5502a4262b5 746f6b656e");
+
+    pub const EXPECTED_TAG: [u8; 16] = hex!("59756519dd6cc85bd90e33a934d2ff85");
+
+    pub const DCID: [u8; 0] = hex!("");
+
+    pub const SCID: [u8; 8] = hex!("f067a5502a4262b5");
+
+    //= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#A
+    //# These packets use an 8-byte client-chosen Destination Connection ID
+    //# of 0x8394c8f03e515708.
+
+    pub const ODCID: [u8; 8] = hex!("8394c8f03e515708");
+
+    pub const VERSION: u32 = 0xff002000;
+
+    pub const TOKEN: [u8; 5] = hex!("746f6b656e");
+}
