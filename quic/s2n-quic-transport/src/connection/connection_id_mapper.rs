d21a2ec6c13df3143ce88d7a1d60ac3451707163
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2020-11-24 09:27:20 -0800

    Store sequence number and expiration timestamp for local connection IDs (#269)
    * Store sequence number and expiration timestamp for local connection IDs
    
    * Store sequence number and expiration timestamp for local connection IDs

--- quic/s2n-quic-transport/src/connection/connection_id_mapper.rs
+++ quic/s2n-quic-transport/src/connection/connection_id_mapper.rs
@@ -3,7 +3,7 @@
 use crate::connection::InternalConnectionId;
 use alloc::rc::Rc;
 use core::cell::RefCell;
-use s2n_quic_core::connection;
+use s2n_quic_core::{connection, time::Timestamp};
 use smallvec::SmallVec;
 use std::collections::hash_map::{Entry, HashMap};
 
@@ -70,6 +70,7 @@ impl ConnectionIdMapper {
             internal_id,
             state: self.state.clone(),
             registered_ids: SmallVec::new(),
+            next_sequence_number: 0,
         }
     }
 }
@@ -88,12 +89,24 @@ pub struct ConnectionIdMapperRegistration {
     /// The shared state between mapper and registration
     state: Rc<RefCell<ConnectionIdMapperState>>,
     /// The connection IDs which are currently registered at the ConnectionIdMapper
-    registered_ids: SmallVec<[connection::Id; NR_STATIC_REGISTRABLE_IDS]>,
+    registered_ids: SmallVec<[LocalConnectionIdInfo; NR_STATIC_REGISTRABLE_IDS]>,
+    next_sequence_number: u32,
+}
+
+#[derive(Debug)]
+struct LocalConnectionIdInfo {
+    id: connection::Id,
+    //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1.1
+    //# Each Connection ID has an associated sequence number to assist in
+    //# detecting when NEW_CONNECTION_ID or RETIRE_CONNECTION_ID frames refer
+    //# to the same value.
+    sequence_number: u32,
+    expiration: Option<Timestamp>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum ConnectionIdMapperRegistrationError {
-    /// The Connection ID had already been registered by another connection
+    /// The Connection ID had already been registered
     ConnectionIdInUse,
 }
 
@@ -102,8 +115,8 @@ impl Drop for ConnectionIdMapperRegistration {
         let mut guard = self.state.borrow_mut();
 
         // Unregister all previously registered IDs
-        for id in &self.registered_ids {
-            guard.connection_map.remove(&id);
+        for id_info in &self.registered_ids {
+            guard.connection_map.remove(&id_info.id);
         }
     }
 }
@@ -114,23 +127,21 @@ impl ConnectionIdMapperRegistration {
         self.internal_id
     }
 
-    //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1.1
-    //= type=TODO
-    //# The sequence number on
-    //# each newly issued connection ID MUST increase by 1.
-    // TODO: Store sequence number along with connection ID
-
-    /// Registers a connection ID mapping at the mapper.
+    /// Registers a connection ID mapping at the mapper with an optional expiration
+    /// timestamp. Returns the sequence number of the connection ID.
     ///
-    /// This will return an error if the provided ConnectionId is already used
-    /// by a different internal connection.
+    /// This will return an error if the provided ConnectionId has already been
+    /// registered or is already used by a different internal connection.
     pub fn register_connection_id(
         &mut self,
         id: &connection::Id,
-    ) -> Result<(), ConnectionIdMapperRegistrationError> {
-        if self.registered_ids.contains(&id) {
-            // Nothing to do
-            return Ok(());
+        expiration: Option<Timestamp>,
+    ) -> Result<u32, ConnectionIdMapperRegistrationError> {
+        if self.registered_ids.iter().any(|id_info| id_info.id == *id) {
+            //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1
+            //# As a trivial example, this means the same connection ID
+            //# MUST NOT be issued more than once on the same connection.
+            return Err(ConnectionIdMapperRegistrationError::ConnectionIdInUse);
         }
 
         // TODO: We might want to limit the maximum amount of aliases which
@@ -144,17 +155,30 @@ impl ConnectionIdMapperRegistration {
             .try_insert(id, self.internal_id)
             .is_ok()
         {
-            // Track the inserted connection ID
-            self.registered_ids.push(*id);
-            Ok(())
+            let sequence_number = self.next_sequence_number;
+
+            // Track the inserted connection ID info
+            self.registered_ids.push(LocalConnectionIdInfo {
+                id: *id,
+                sequence_number,
+                expiration,
+            });
+            //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1.1
+            //# The sequence number on
+            //# each newly issued connection ID MUST increase by 1.
+            self.next_sequence_number += 1;
+            Ok(sequence_number)
         } else {
             Err(ConnectionIdMapperRegistrationError::ConnectionIdInUse)
         }
     }
 
     /// Unregisters a connection ID at the mapper
-    pub fn ungister_connection_id(&mut self, id: &connection::Id) {
-        let registration_index = match self.registered_ids.iter().position(|iter_id| iter_id == id)
+    pub fn unregister_connection_id(&mut self, id: &connection::Id) {
+        let registration_index = match self
+            .registered_ids
+            .iter()
+            .position(|id_info| id_info.id == *id)
         {
             Some(index) => index,
             None => return, // Nothing to do
@@ -176,6 +200,42 @@ mod tests {
     use super::*;
     use crate::connection::InternalConnectionIdGenerator;
 
+    //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1
+    //= type=test
+    //# As a trivial example, this means the same connection ID
+    //# MUST NOT be issued more than once on the same connection.
+    #[test]
+    fn same_connection_id_must_not_be_issued_for_same_connection() {
+        let mut reg = ConnectionIdMapper::new()
+            .create_registration(InternalConnectionIdGenerator::new().generate_id());
+
+        let ext_id = connection::Id::try_from_bytes(b"id1").unwrap();
+
+        assert!(reg.register_connection_id(&ext_id, None).is_ok());
+        assert_eq!(
+            Err(ConnectionIdMapperRegistrationError::ConnectionIdInUse),
+            reg.register_connection_id(&ext_id, None)
+        );
+    }
+
+    //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#5.1.1
+    //= type=test
+    //# The sequence number on
+    //# each newly issued connection ID MUST increase by 1.
+    #[test]
+    fn sequence_number_must_increase_by_one() {
+        let mut reg = ConnectionIdMapper::new()
+            .create_registration(InternalConnectionIdGenerator::new().generate_id());
+
+        let ext_id_1 = connection::Id::try_from_bytes(b"id1").unwrap();
+        let ext_id_2 = connection::Id::try_from_bytes(b"id2").unwrap();
+
+        let seq_num_1 = reg.register_connection_id(&ext_id_1, None).unwrap();
+        let seq_num_2 = reg.register_connection_id(&ext_id_2, None).unwrap();
+
+        assert_eq!(1, seq_num_2 - seq_num_1);
+    }
+
     #[test]
     fn connection_mapper_test() {
         let mut id_generator = InternalConnectionIdGenerator::new();
@@ -192,28 +252,35 @@ mod tests {
         let ext_id_3 = connection::Id::try_from_bytes(b"id3").unwrap();
         let ext_id_4 = connection::Id::try_from_bytes(b"id4").unwrap();
 
+        let exp_1 = s2n_quic_platform::time::now();
+
         assert!(mapper.lookup_internal_connection_id(&ext_id_1).is_none());
-        assert!(reg1.register_connection_id(&ext_id_1).is_ok());
-        assert!(reg1.register_connection_id(&ext_id_1).is_ok());
+        let result = reg1.register_connection_id(&ext_id_1, Some(exp_1));
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap(),
+            reg1.registered_ids.get(0).unwrap().sequence_number
+        );
+        assert_eq!(Some(exp_1), reg1.registered_ids.get(0).unwrap().expiration);
         assert_eq!(Some(id1), mapper.lookup_internal_connection_id(&ext_id_1));
 
         assert_eq!(
             Err(ConnectionIdMapperRegistrationError::ConnectionIdInUse),
-            reg2.register_connection_id(&ext_id_1)
+            reg2.register_connection_id(&ext_id_1, None)
         );
 
-        assert!(reg1.register_connection_id(&ext_id_2).is_ok());
-        assert!(reg2.register_connection_id(&ext_id_3).is_ok());
-        assert!(reg2.register_connection_id(&ext_id_4).is_ok());
+        assert!(reg1.register_connection_id(&ext_id_2, None).is_ok());
+        assert!(reg2.register_connection_id(&ext_id_3, None).is_ok());
+        assert!(reg2.register_connection_id(&ext_id_4, None).is_ok());
         assert_eq!(Some(id1), mapper.lookup_internal_connection_id(&ext_id_2));
         assert_eq!(Some(id2), mapper.lookup_internal_connection_id(&ext_id_3));
         assert_eq!(Some(id2), mapper.lookup_internal_connection_id(&ext_id_4));
 
-        reg2.ungister_connection_id(&ext_id_3);
+        reg2.unregister_connection_id(&ext_id_3);
         assert_eq!(None, mapper.lookup_internal_connection_id(&ext_id_3));
         assert_eq!(Some(id2), mapper.lookup_internal_connection_id(&ext_id_4));
 
-        assert!(reg1.register_connection_id(&ext_id_3).is_ok());
+        assert!(reg1.register_connection_id(&ext_id_3, None).is_ok());
         assert_eq!(Some(id1), mapper.lookup_internal_connection_id(&ext_id_3));
 
         // If a registration is dropped all entries are removed
