db22334b79ec60c8c08df977b8ffb0bc39231a22
Cameron Bytheway <bytheway.cameron@gmail.com>
2020-11-24 16:46:44 -0800

    core: implement checked counters

--- quic/s2n-quic-core/src/lib.rs
+++ quic/s2n-quic-core/src/lib.rs
@@ -3,11 +3,13 @@
 pub mod ack_set;
 pub mod application;
 pub mod connection;
+pub mod counter;
 pub mod crypto;
 pub mod endpoint;
 pub mod frame;
 pub mod inet;
 pub mod io;
+pub mod number;
 pub mod packet;
 pub mod path;
 pub mod recovery;
