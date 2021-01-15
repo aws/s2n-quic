3dcd035f08b1617f5a640bce9063dc20dfa576ac
Cameron Bytheway <bytheway.cameron@gmail.com>
2021-01-15 13:07:38 -0800

    clean up remaining bits

--- tls/s2n-tls-sys/s2n-sys.h
+++
@@ -1,5 +0,0 @@
-#include <s2n.h>
-
-#ifdef _S2N_QUIC_SUPPORT
-#include "s2n/tls/s2n_quic_support.h"
-#endif
--- tls/s2n-tls-sys/update.sh
+++ tls/s2n-tls-sys/update.sh
@@ -2,17 +2,5 @@
 
 set -e
 
-OUT=$(pwd)
-TMP=$(mktemp -d)
-
-VERSION=${1:-main}
-REPO=${2:-https://github.com/awslabs/s2n.git}
-
-#git clone $REPO $TMP
-#cd $TMP
-#git checkout $VERSION
-#S2N=$TMP
-S2N=$OUT/s2n
-
 cargo run --manifest-path update/Cargo.toml
 
