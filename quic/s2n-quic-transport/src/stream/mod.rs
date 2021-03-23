3dd6064458abc49d92c14b3922774d334577035b
Wesley Rosenblum <wesleyr@amazon.com>
2021-03-23 14:42:13 -0700

    Fix formatting

--- quic/s2n-quic-transport/src/stream/mod.rs
+++ quic/s2n-quic-transport/src/stream/mod.rs
@@ -5,9 +5,9 @@
 
 mod api;
 mod incoming_connection_flow_controller;
-mod peer_controller;
-mod outgoing_connection_flow_controller;
 mod local_controller;
+mod outgoing_connection_flow_controller;
+mod peer_controller;
 mod receive_stream;
 mod send_stream;
 mod stream_container;
@@ -20,12 +20,12 @@ mod stream_manager;
 pub(crate) mod contract;
 
 pub use api::*;
+pub use local_controller::LocalController;
+pub use peer_controller::PeerController;
 pub use s2n_quic_core::stream::limits::Limits;
 pub use stream_events::StreamEvents;
 pub use stream_impl::{StreamImpl, StreamTrait};
 pub use stream_manager::AbstractStreamManager;
-pub use peer_controller::PeerController;
-pub use local_controller::LocalController;
 
 pub type StreamManager = AbstractStreamManager<StreamImpl>;
 
