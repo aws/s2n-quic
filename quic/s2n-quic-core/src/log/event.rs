6a66ddb16ec5666bf31b9276b837c5753bf704c2
Apoorv Kothari <apoorvko@amazon.com>
2021-04-09 15:38:16 -0700

    make name a static str

--- quic/s2n-quic-core/src/log/event.rs
+++ quic/s2n-quic-core/src/log/event.rs
@@ -6,25 +6,34 @@ macro_rules! impl_event {
         impl Event for $name {}
 
         impl Meta for $name{
-            fn name<'a>() -> &'a str {
-                $qlog_name
+            fn name(&self) -> &str {
+                self.name
             }
         }
     };
 }
 
+/// The Event trait marks all possible types of events that can be emitted by
+/// the library.
 pub trait Event: Meta {}
 
+/// This is meant to capture common meta values that are shared across events.
+/// Some of these are included to maintain compatibility with the main qlog
+/// rfc.
 pub trait Meta {
-    fn name<'a>() -> &'a str;
+    fn name(&self) -> &str;
 }
 
 #[non_exhaustive]
-pub struct VersionInformation{}
+pub struct VersionInformation{
+    name: &'static str
+}
 impl_event!(
     VersionInformation, "transport:version_information"
 );
 
 #[non_exhaustive]
-pub struct AlpnInformation{}
+pub struct AlpnInformation{
+    name: &'static str
+}
 impl_event!(AlpnInformation, "transport:alpn_information");
