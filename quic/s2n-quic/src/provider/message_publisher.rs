06df5994524f33e524e662043015dd7e9b942916
Apoorv Kothari <apoorvko@amazon.com>
2021-04-10 22:32:58 -0700

    include provider/message_publisher

---
+++ quic/s2n-quic/src/provider/message_publisher.rs
@@ -0,0 +1,44 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+pub use crate::message::Publisher;
+
+pub use s2n_quic_core::connection::limits::{ConnectionInfo, Limiter, Limits};
+
+/// Provides logging support for an endpoint
+pub trait Provider {
+    type Publisher: 'static + Publisher;
+    // type Publisher: 'static;
+    type Error: 'static + core::fmt::Display;
+
+    fn start(self) -> Result<Self::Publisher, Self::Error>;
+}
+
+pub use default::Provider as Default;
+
+impl_provider_utils!();
+
+pub mod default {
+    pub use crate::message::event::*;
+
+    #[derive(Debug, Default)]
+    pub struct Provider;
+
+    impl super::Provider for Provider {
+        type Publisher = Publisher; // TODO
+        type Error = core::convert::Infallible;
+
+        fn start(self) -> Result<Self::Publisher, Self::Error> {
+            // TODO
+            Ok(Publisher)
+        }
+    }
+
+    pub struct Publisher;
+
+    impl super::Publisher for Publisher {
+        fn on_version_information(&self, event: &VersionInformation) {
+            let _ = event;
+        }
+    }
+}
