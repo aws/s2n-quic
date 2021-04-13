c72ff138e4e4df5ad601d8c8247903cd0f94ad13
Apoorv Kothari <apoorvko@amazon.com>
2021-04-12 17:48:26 -0700

    add comments

--- quic/s2n-quic-core/src/event.rs
+++ quic/s2n-quic-core/src/event.rs
@@ -7,10 +7,13 @@ use paste::paste;
 #[macro_use]
 mod macros;
 
+/// All events types which can be emitted from this library.
 pub trait Event {
     const NAME: &'static str;
 }
 
+/// Common fields that are common to all events. Some of these fields exits to
+/// maintain compatibility with the qlog spec.
 #[derive(Clone, Debug)]
 pub struct Meta {
     pub vantage_point: endpoint::Type,
@@ -19,18 +22,22 @@ pub struct Meta {
 
 events!(
     #[name = "transport::version_information"]
+    // https://tools.ietf.org/html/draft-marx-qlog-event-definitions-quic-h3-02#section-5.3.1
+    /// QUIC version
     struct VersionInformation<'a> {
-        pub server: &'a [u32],
-        pub client: &'a [u32],
-        pub chosen: u32,
+        pub server_versions: &'a [u32],
+        pub client_versions: &'a [u32],
+        pub chosen_version: u32,
         pub meta: Meta,
     }
 
     #[name = "transport:alpn_information"]
+    // https://tools.ietf.org/html/draft-marx-qlog-event-definitions-quic-h3-02#section-5.3.1
+    /// Application level protocol
     struct AlpnInformation<'a> {
-        pub server: &'a [&'a [u8]],
-        pub client: &'a [&'a [u8]],
-        pub chosen: u32,
+        pub server_alpns: &'a [&'a [u8]],
+        pub client_alpns: &'a [&'a [u8]],
+        pub chosen_alpn: u32,
         pub meta: Meta,
     }
 );
--- quic/s2n-quic-core/src/event/macros.rs
+++ quic/s2n-quic-core/src/event/macros.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 macro_rules! events {
     ($(
         #[name = $name_str:literal]
