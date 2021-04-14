b4390892d5ec5d46170694b012cafb9fb745d934
toidiu <apoorv@toidiu.com>
2021-04-14 13:49:02 -0700

    integrate tracing for logging (#612)

---
+++ quic/s2n-quic/src/provider/event/mod.rs
@@ -0,0 +1,26 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use cfg_if::cfg_if;
+pub use s2n_quic_core::event::{events, Subscriber};
+
+/// Provides logging support for an endpoint
+pub trait Provider {
+    type Subscriber: 'static + Subscriber;
+    type Error: 'static + core::fmt::Display;
+
+    fn start(self) -> Result<Self::Subscriber, Self::Error>;
+}
+
+cfg_if! {
+    if #[cfg(feature = "tracing-provider")] {
+        pub use self::tracing as default;
+        pub mod tracing;
+    } else {
+        pub mod default;
+    }
+}
+
+pub use default::Provider as Default;
+
+impl_provider_utils!();
---
+++ quic/s2n-quic/src/provider/event/tracing.rs
@@ -0,0 +1,30 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use s2n_quic_core::event::events;
+use tracing::info;
+
+#[derive(Debug, Default)]
+pub struct Provider;
+
+impl super::Provider for Provider {
+    type Subscriber = Subscriber;
+    type Error = core::convert::Infallible;
+
+    fn start(self) -> Result<Self::Subscriber, Self::Error> {
+        Ok(Subscriber)
+    }
+}
+
+pub struct Subscriber;
+
+// TODO we should implement Display for Events or maybe opt into serde as a feature
+impl super::Subscriber for Subscriber {
+    fn on_version_information(&mut self, event: &events::VersionInformation) {
+        info!("{:?}", event);
+    }
+
+    fn on_alpn_information(&mut self, event: &events::AlpnInformation) {
+        info!("{:?}", event);
+    }
+}
