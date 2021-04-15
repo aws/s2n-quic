f63b800c676b1c47f459a0e0e3935cc5313f9fdf
Apoorv Kothari <apoorvko@amazon.com>
2021-04-14 17:16:26 -0700

    match fields

--- quic/s2n-quic-core/src/event/macros.rs
+++ quic/s2n-quic-core/src/event/macros.rs
@@ -9,24 +9,51 @@ struct Foo<'a> {
     pub meta: super::Meta,
 }
 
+#[allow(dead_code)]
+impl<'a> Foo<'a> {
+    pub fn builder() -> FooMetaBuilder<'a> {
+        FooMetaBuilder(Foo::default())
+    }
+}
+
+struct FooMetaBuilder<'a>(Foo<'a>);
+
+#[allow(dead_code)]
+impl<'a> FooMetaBuilder<'a> {
+    pub fn with_meta(self, meta: super::Meta) -> FooBuilder<'a> {
+        let mut event = self.0;
+        event.meta = meta;
+        FooBuilder(event)
+    }
+
+    pub fn without_meta(self) -> FooBuilder<'a> {
+        let event = self.0;
+        FooBuilder(event)
+    }
+}
+
 struct FooBuilder<'a>(Foo<'a>);
 
+#[allow(dead_code)]
 impl<'a> FooBuilder<'a> {
-    #[allow(dead_code)]
+    fn with_server_alpns(&mut self) {
+    }
+
     fn build(self) -> Foo<'a> {
         self.0
     }
 }
 
-
-
+fn _bla() {
+    let _e = Foo::builder().without_meta().build();
+}
 
 macro_rules! events {
     ($(
         #[name = $name_str:literal]
         $(#[$attrs:meta])*
         struct $name:ident $(<$lt:lifetime>)? {
-            $($fields:tt)*
+            $( pub $field_name:ident : $field_type:ty, )*
         }
     )*) => {
         pub mod events {
@@ -37,7 +64,7 @@ macro_rules! events {
                 #[non_exhaustive]
                 #[derive(Clone, Debug, Default)]
                 pub struct $name $(<$lt>)? {
-                    $($fields)*
+                    $( pub $field_name: $field_type, )*
                 }
 
                 impl $(<$lt>)? Event for $name $(<$lt>)? {
@@ -47,17 +74,35 @@ macro_rules! events {
                 paste! {
                     impl $(<$lt>)? $name $(<$lt>)? {
                         pub fn builder() -> [<$name Builder>] $(<$lt>)? {
-                            [<$name Builder>]::default()
+                            [<$name Builder>] ($name::default())
                         }
                     }
 
-                    #[derive(Clone, Debug, Default)]
+                    #[derive(Clone, Debug)]
+                    pub struct [<$name MetaBuilder>] $(<$lt>)? (
+                        $name $(<$lt>)?
+                    );
+
+                    #[allow(dead_code)]
+                    impl $(<$lt>)? [<$name MetaBuilder>] $(<$lt>)? {
+                        pub fn with_meta(self, meta: super::Meta) -> [<$name Builder>] $(<$lt>)? {
+                            let mut event = self.0;
+                            event.meta = meta;
+                            [<$name Builder>] (event)
+                        }
+
+                        pub fn without_meta(self) -> [<$name Builder>] $(<$lt>)? {
+                            [<$name Builder>] (self.0)
+                        }
+                    }
+
+                    #[derive(Clone, Debug)]
                     pub struct [<$name Builder>] $(<$lt>)? (
                         $name $(<$lt>)?
                     );
 
+                    #[allow(dead_code)]
                     impl $(<$lt>)? [<$name Builder>] $(<$lt>)? {
-                        #[allow(dead_code)]
                         fn build(self) -> $name $(<$lt>)? {
                             self.0
                         }
