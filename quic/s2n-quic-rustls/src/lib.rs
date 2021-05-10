2db4cc48dd44731f270541503b6d0b8408ae76bc
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2021-05-10 13:58:51 -0700

    Allow cert_resolver to be specified for Rustls and reverse default Cipher preferences (#653)
    * Add with_cert_resolver to Rustls server builder and reverse order of cipher preferences to prefer AES128
    
    * Update comment

--- quic/s2n-quic-rustls/src/lib.rs
+++ quic/s2n-quic-rustls/src/lib.rs
@@ -32,11 +32,20 @@ pub mod certificate;
 
 // The first 3 ciphers are TLS1.3
 // https://github.com/ctz/rustls/blob/1287510bece905b7e45cf31d6e7cf3334b98bb2e/rustls/src/suites.rs#L379
-// We fetch the top 3 ciphersuites from rustls::ALL_CIPHERSUITES. This is an internal implementation
-// and can change in future versions of rustls. Therefore care must be taken to maintain the same order
-// of ciphersuites when upgrading rustls version.
+// We fetch the top 3 ciphersuites from rustls::ALL_CIPHERSUITES and reverse the order to end up
+// with a default preference of:
+// 1. TLS13_AES_128_GCM_SHA256
+// 2. TLS13_AES_256_GCM_SHA384
+// 3. TLS13_CHACHA20_POLY1305_SHA256
+// This is an internal implementation and can change in future versions of rustls. Therefore care
+// must be taken to maintain the same order of ciphersuites when upgrading rustls version.
 pub fn default_ciphersuites() -> Vec<&'static SupportedCipherSuite> {
-    rustls::ALL_CIPHERSUITES.iter().take(3).cloned().collect()
+    rustls::ALL_CIPHERSUITES
+        .iter()
+        .take(3)
+        .cloned()
+        .rev()
+        .collect()
 }
 
 //= https://tools.ietf.org/id/draft-ietf-quic-tls-32.txt#4.2
@@ -351,7 +360,7 @@ macro_rules! impl_tls {
 
 pub mod server {
     use super::*;
-    pub use rustls::ServerConfig as Config;
+    pub use rustls::{ResolvesServerCert, ServerConfig as Config};
 
     impl_tls!(Server, Session, ServerConfig, ServerSession, new_server);
 
@@ -409,6 +418,14 @@ pub mod server {
             let certificate = certificate.into_certificate()?;
             let private_key = private_key.into_private_key()?;
             self.config.set_single_cert(certificate.0, private_key.0)?;
+            Ok(self)
+        }
+
+        pub fn with_cert_resolver(
+            mut self,
+            cert_resolver: Arc<dyn ResolvesServerCert>,
+        ) -> Result<Self, TLSError> {
+            self.config.cert_resolver = cert_resolver;
             Ok(self)
         }
 
@@ -627,7 +644,7 @@ fn test_default_ciphersuites() {
     let ciphersuites = default_ciphersuites();
     assert_eq!(
         ciphersuites.get(0).unwrap().suite,
-        rustls::CipherSuite::TLS13_CHACHA20_POLY1305_SHA256
+        rustls::CipherSuite::TLS13_AES_128_GCM_SHA256
     );
     assert_eq!(
         ciphersuites.get(1).unwrap().suite,
@@ -635,7 +652,7 @@ fn test_default_ciphersuites() {
     );
     assert_eq!(
         ciphersuites.get(2).unwrap().suite,
-        rustls::CipherSuite::TLS13_AES_128_GCM_SHA256
+        rustls::CipherSuite::TLS13_CHACHA20_POLY1305_SHA256
     );
 }
 
