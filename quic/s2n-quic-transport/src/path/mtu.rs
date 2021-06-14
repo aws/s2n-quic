50860dd74c7651ab20fadda63d9a0f81e9baa053
Wesley Rosenblum <wesleyr@amazon.com>
2021-06-14 15:08:59 -0700

    Remove state diagram and fix typo

--- quic/s2n-quic-transport/src/path/mtu.rs
+++ quic/s2n-quic-transport/src/path/mtu.rs
@@ -15,46 +15,6 @@ use s2n_quic_core::{
     time::Timestamp,
 };
 
-//= https://tools.ietf.org/rfc/rfc8899.txt#5.2
-//#    |         |
-//#    | Start   | PL indicates loss
-//#    |         |  of connectivity
-//#    v         v
-//# +---------------+                                   +---------------+
-//# |    DISABLED   |                                   |     ERROR     |
-//# +---------------+               PROBE_TIMER expiry: +---------------+
-//#         | PL indicates     PROBE_COUNT = MAX_PROBES or    ^      |
-//#         | connectivity  PTB: PL_PTB_SIZE < BASE_PLPMTU    |      |
-//#         +--------------------+         +------------------+      |
-//#                              |         |                         |
-//#                              v         |       BASE_PLPMTU Probe |
-//#                           +---------------+          acked       |
-//#                           |      BASE     |--------------------->+
-//#                           +---------------+                      |
-//#                              ^ |    ^  ^                         |
-//#          Black hole detected | |    |  | Black hole detected     |
-//#         +--------------------+ |    |  +--------------------+    |
-//#         |                      +----+                       |    |
-//#         |                PROBE_TIMER expiry:                |    |
-//#         |             PROBE_COUNT < MAX_PROBES              |    |
-//#         |                                                   |    |
-//#         |               PMTU_RAISE_TIMER expiry             |    |
-//#         |    +-----------------------------------------+    |    |
-//#         |    |                                         |    |    |
-//#         |    |                                         v    |    v
-//# +---------------+                                   +---------------+
-//# |SEARCH_COMPLETE|                                   |   SEARCHING   |
-//# +---------------+                                   +---------------+
-//#    |    ^    ^                                         |    |    ^
-//#    |    |    |                                         |    |    |
-//#    |    |    +-----------------------------------------+    |    |
-//#    |    |            MAX_PLPMTU Probe acked or              |    |
-//#    |    |  PROBE_TIMER expiry: PROBE_COUNT = MAX_PROBES or  |    |
-//#    +----+            PTB: PL_PTB_SIZE = PLPMTU              +----+
-//# CONFIRMATION_TIMER expiry:                        PROBE_TIMER expiry:
-//# PROBE_COUNT < MAX_PROBES or               PROBE_COUNT < MAX_PROBES or
-//#      PLPMTU Probe acked                           Probe acked or PTB:
-//#                                    PLPMTU < PL_PTB_SIZE < PROBED_SIZE
 #[derive(Debug, PartialEq, Eq)]
 enum State {
     //= https://tools.ietf.org/rfc/rfc8899.txt#5.2
@@ -135,6 +95,11 @@ pub struct Controller {
     //= https://tools.ietf.org/rfc/rfc8899.txt#5.1.2
     //# The MAX_PLPMTU is the largest size of PLPMTU.
     max_plpmtu: u16,
+    //= https://tools.ietf.org/rfc/rfc8899.txt#5.1.3
+    //# The PROBED_SIZE is the size of the current probe packet
+    //# as determined at the PL.  This is a tentative value for the
+    //# PLPMTU, which is awaiting confirmation by an acknowledgment.
+    probed_size: u16,
     // The maximum size datagram to probe for. In contrast to the max_plpmtu,
     // this value will decrease if probes are not acknowledged.
     max_probe_size: u16,
@@ -142,11 +107,6 @@ pub struct Controller {
     //# The PROBE_COUNT is a count of the number of successive
     //# unsuccessful probe packets that have been sent.
     probe_count: u8,
-    //= https://tools.ietf.org/rfc/rfc8899.txt#5.1.3
-    //# The PROBED_SIZE is the size of the current probe packet
-    //# as determined at the PL.  This is a tentative value for the
-    //# PLPMTU, which is awaiting confirmation by an acknowledgment.
-    probed_size: u16,
     //= https://tools.ietf.org/rfc/rfc8899.txt#5.1.1
     //# The PMTU_RAISE_TIMER is configured to the period a
     //# sender will continue to use the current PLPMTU, after which it
@@ -178,10 +138,10 @@ impl Controller {
         Self {
             state: State::Disabled,
             plpmtu: BASE_PLPMTU,
+            probed_size: initial_probed_size,
             max_plpmtu,
             max_probe_size: max_plpmtu,
             probe_count: 0,
-            probed_size: initial_probed_size,
             pmtu_raise_timer: VirtualTimer::default(),
         }
     }
@@ -263,7 +223,7 @@ impl Controller {
     /// Queries the component for any outgoing frames that need to get sent
     ///
     /// This method assumes that no other data (other than the packet header) has been written
-    /// to the supplied `WriteContext`. The necessitates the caller ensuring the probe packet
+    /// to the supplied `WriteContext`. This necessitates the caller ensuring the probe packet
     /// written by this method to be in its own connection transmission.
     pub fn on_transmit<W: WriteContext>(&mut self, context: &mut W) -> Result<(), OnTransmitError> {
         if !matches!(self.state, State::SearchRequested) {
