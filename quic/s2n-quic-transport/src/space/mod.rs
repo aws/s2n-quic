df94d1cb3511daa2aff1afa42e760e3c872579b2
toidiu <apoorv@toidiu.com>
2021-06-29 14:35:22 -0700

    some tweaks in on_non_probing_packet

--- quic/s2n-quic-transport/src/space/mod.rs
+++ quic/s2n-quic-transport/src/space/mod.rs
@@ -676,7 +676,7 @@ pub trait PacketSpace<Config: endpoint::Config> {
             payload = remaining;
         }
         if is_path_validation_probing.is_probing() {
-            path_manager.on_non_probing_packet(path_id)?;
+            path_manager.on_non_path_validation_probing_packet(path_id)?;
         }
 
         //= https://tools.ietf.org/id/draft-ietf-quic-transport-32.txt#13.1
