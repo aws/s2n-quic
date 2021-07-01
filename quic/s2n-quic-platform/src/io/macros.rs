d52c1626dd33e5cf1b180d964499cb1a885e2bab
Wesley Rosenblum <wesleyr@amazon.com>
2021-07-01 14:14:24 -0700

    Error on libc return codes less than zero

--- quic/s2n-quic-platform/src/io/macros.rs
+++ quic/s2n-quic-platform/src/io/macros.rs
@@ -7,7 +7,7 @@
 macro_rules! libc {
     ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
         let res = unsafe { libc::$fn($($arg, )*) };
-        if res == -1 {
+        if res < 0 {
             Err(std::io::Error::last_os_error())
         } else {
             Ok(res)
