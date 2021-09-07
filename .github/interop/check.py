2779bb4f8ed6431d351a324c689b5e8846022147
Cameron Bytheway <bytheway.cameron@gmail.com>
2021-09-07 12:21:35 -0700

    ci: add interop checks

---
+++ .github/interop/check.py
@@ -0,0 +1,76 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import json
+import itertools
+import sys
+
+parser = argparse.ArgumentParser(description='Check interop reports.')
+parser.add_argument('--required', type=str)
+parser.add_argument('--regressions')
+parser.add_argument('report')
+args = parser.parse_args()
+
+status = {'ok': True, 'failures': 0}
+def fail(message):
+    print(message)
+    status['ok'] = False
+    status['failures'] += 1
+
+def format_required_report(report):
+    outcome = {}
+    result_idx = 0
+
+    # find the version of s2n-quic to check
+    s2n_quic = 's2n-quic'
+    for impl in itertools.chain(report['clients'], report['servers']):
+        # if we're testing a PR then use that name
+        if impl.startswith('s2n-quic-pr'):
+            s2n_quic = impl
+            break
+
+    for client in report['clients']:
+        for server in report['servers']:
+            result = report['results'][result_idx]
+            result_idx += 1
+
+            # we're only interested in s2n-quic results
+            if client != s2n_quic and server != s2n_quic:
+                continue
+
+            for test in result:
+                outcome.setdefault(test['name'], {})
+
+                info = outcome[test['name']]
+                info.setdefault(client, {'client': False, 'server': False})
+                info.setdefault(server, {'client': False, 'server': False})
+
+                success = test['result'] == 'succeeded'
+                info[client]['client'] = success
+                info[server]['server'] = success
+
+    return outcome
+
+with open(args.report) as f:
+    result = json.load(f)
+
+    if args.regressions and result['regression']:
+        fail("A regression from main was detected")
+
+    if args.required:
+        with open(args.required) as r:
+            required = json.load(r)
+
+            actual = format_required_report(result)
+
+            for test, impls in required.items():
+                test_results = actual[test]
+                for impl_name, endpoints in impls.items():
+                    impl = test_results[impl_name]
+                    for endpoint in endpoints:
+                        if not impl[endpoint]:
+                            fail("{} ({}) - {} was expected to pass but failed".format(impl_name, endpoint, test))
+
+if not status['ok']:
+    sys.exit(status['failures'])
---
+++ .github/interop/required.check.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+N_COMMITS=${1:-50}
+
+# get a list of N commit hashes
+COMMITS=$(git log main -n $N_COMMITS --format="format:%H")
+OUT_DIR=target/interop/results
+
+# list of broken commits that should be ignored
+SKIPLIST="
+f1193adee0fb3e39d5d1142c5bc2bf01943edada
+51d46666a3b3a9d9b0d5c58636dda3c4e043d256
+d418b19c4b90952ca0f3e940e74b93fba0e724fa
+"
+
+mkdir -p $OUT_DIR
+
+IFS=$'\n'
+for commit in $COMMITS; do
+  rm -f result.json
+
+  if [[ "$SKIPLIST" =~ (^|[[:space:]])$commit($|[[:space:]]) ]]; then
+    echo "$commit in skiplist"
+    continue
+  fi
+
+  echo $commit
+
+  if [ ! -f $OUT_DIR/$commit.json ]; then
+    curl --fail --silent -o $OUT_DIR/$commit.json https://dnglbrstg7yg.cloudfront.net/$commit/interop/logs/latest/result.json || touch $OUT_DIR/$commit.json
+  fi
+
+  if [ -s $OUT_DIR/$commit.json ]; then
+    python .github/interop/check.py --required .github/interop/required.json $OUT_DIR/$commit.json
+  else
+    echo "    report could not be found; skipping"
+  fi
+done
