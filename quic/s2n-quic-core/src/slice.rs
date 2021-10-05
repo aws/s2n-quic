75de2181fcff356ff2b883faa4415de9145fd4cc
Angelo Nakos <3038292+angelonakos@users.noreply.github.com>
2021-10-05 00:33:02 +0000

    Add Python script to invoke RMC in CI

--- quic/s2n-quic-core/src/slice.rs
+++ quic/s2n-quic-core/src/slice.rs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-include!("/home/ubuntu/tools/rmc/src/test/rmc-prelude.rs");
+include!("/home/ubuntu/rmc/src/test/rmc-prelude.rs");
 
 use std::ops::{Deref, DerefMut};
 
