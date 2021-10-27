58a702fd9dbfa253b27dc3d7e6d4406ea9121fef
Cameron Bytheway <bythewc@amazon.com>
2021-10-27 12:25:38 -0600

    bench: move all benchmarks to a single crate

---
+++ quic/s2n-quic-bench/src/packet.rs
@@ -0,0 +1,61 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use criterion::{black_box, BenchmarkId, Criterion, Throughput};
+use s2n_codec::DecoderBufferMut;
+use s2n_quic_core::{connection::id::ConnectionInfo, inet::SocketAddress, packet::ProtectedPacket};
+
+pub fn benchmarks(c: &mut Criterion) {
+    codec(c);
+}
+
+struct Input {
+    name: &'static str,
+    buffer: &'static [u8],
+}
+
+macro_rules! input {
+    ($name:expr) => {
+        Input {
+            name: $name,
+            buffer: include_bytes!(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/../s2n-quic-core/src/packet/test_samples/",
+                $name,
+                ".bin"
+            )),
+        }
+    };
+}
+
+fn codec(c: &mut Criterion) {
+    let mut group = c.benchmark_group("packet");
+
+    let inputs = [
+        input!("handshake"),
+        input!("initial"),
+        input!("short"),
+        input!("zero_rtt"),
+        input!("version_negotiation"),
+        input!("retry"),
+    ];
+
+    for input in &inputs {
+        group.throughput(Throughput::Bytes(input.buffer.len() as _));
+        group.bench_with_input(
+            BenchmarkId::new("decode", input.name),
+            &input.buffer,
+            |b, buffer| {
+                let mut buffer = buffer.to_vec();
+                b.iter(move || {
+                    let buffer = DecoderBufferMut::new(&mut buffer);
+                    let remote_address = SocketAddress::default();
+                    let connection_info = ConnectionInfo::new(&remote_address);
+                    let _ = black_box(ProtectedPacket::decode(buffer, &connection_info, &20));
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
---
+++ quic/s2n-quic-bench/src/varint.rs
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use criterion::{black_box, BenchmarkId, Criterion};
+use s2n_codec::{DecoderBuffer, EncoderBuffer, EncoderValue};
+use s2n_quic_core::varint::VarInt;
+
+pub fn benchmarks(c: &mut Criterion) {
+    round_trip(c);
+}
+
+fn round_trip(c: &mut Criterion) {
+    let mut group = c.benchmark_group("varint");
+    for i in [0, 1, 5, 6, 13, 14, 29, 30, 61].iter() {
+        let i = VarInt::new(2u64.pow(*i)).unwrap();
+
+        group.bench_with_input(BenchmarkId::new("round_trip", i), &i, |b, input| {
+            let mut buffer = vec![0; 8];
+            b.iter(|| {
+                input.encode(&mut EncoderBuffer::new(&mut buffer));
+                let (actual, _) = DecoderBuffer::new(&buffer).decode::<VarInt>().unwrap();
+                black_box(actual);
+            });
+        });
+    }
+    group.finish();
+}
