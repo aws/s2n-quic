26f7c6a3b8d9455e296bb02a4c295dd683038108
toidiu <apoorv@toidiu.com>
2021-11-12 16:42:01 -0800

    rustls

--- quic/s2n-quic-tls-default/src/lib.rs
+++ quic/s2n-quic-tls-default/src/lib.rs
@@ -1,7 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(not(unix))]
+// #[cfg(not(unix))]
+// pub use s2n_quic_rustls::*;
+// #[cfg(unix)]
+// pub use s2n_quic_tls::*;
 pub use s2n_quic_rustls::*;
-#[cfg(unix)]
-pub use s2n_quic_tls::*;
