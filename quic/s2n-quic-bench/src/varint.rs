ac1b1e705e965093cd50b04ba57f0a5aa24b2eaf
WesleyRosenblum <wesleyr@amazon.com>
2021-12-10 13:45:55 -0800

    Fix integer dereference

--- quic/s2n-quic-bench/src/varint.rs
+++ quic/s2n-quic-bench/src/varint.rs
@@ -12,7 +12,7 @@ pub fn benchmarks(c: &mut Criterion) {
 fn round_trip(c: &mut Criterion) {
     let mut group = c.benchmark_group("varint");
     for i in [0, 1, 5, 6, 13, 14, 29, 30, 61] {
-        let i = VarInt::new(2u64.pow(*i)).unwrap();
+        let i = VarInt::new(2u64.pow(i)).unwrap();
 
         group.bench_with_input(BenchmarkId::new("round_trip", i), &i, |b, input| {
             let mut buffer = vec![0; 8];
