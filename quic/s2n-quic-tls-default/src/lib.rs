72af27f7b2228e8bdad727134c0e69ddc0be0211
toidiu <apoorv@toidiu.com>
2022-01-04 12:45:51 -0800

    test with rustls

--- quic/s2n-quic-tls-default/src/lib.rs
+++ quic/s2n-quic-tls-default/src/lib.rs
@@ -1,7 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(not(unix))]
 pub use s2n_quic_rustls::*;
-#[cfg(unix)]
-pub use s2n_quic_tls::*;
