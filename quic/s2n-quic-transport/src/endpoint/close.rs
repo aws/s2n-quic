e38a748de800b2fb8547a4a5257e9029c50f628a
toidiu <apoorv@toidiu.com>
2022-01-05 11:06:34 -0800

    use simplified is_open state for driving close

--- quic/s2n-quic-transport/src/endpoint/close.rs
+++ quic/s2n-quic-transport/src/endpoint/close.rs
@@ -10,32 +10,20 @@ use core::{
     task::Poll,
 };
 
-enum CloseState {
-    /// Send a close request to the endpoint
-    SendCloseAttempt {
-        close_sender: CloseSender,
-        is_open: Arc<AtomicBool>,
-    },
-    /// Wait for the endpoint to gracefully close and notify this future
-    AwaitEndpointClose {
-        is_open: Arc<AtomicBool>,
-    },
-    Unreachable,
-}
-
 #[must_use = "futures do nothing unless you `.await` or poll them"]
 pub struct Attempt {
-    state: CloseState,
+    request_sent: bool,
+    close_sender: CloseSender,
+    is_open: Arc<AtomicBool>,
 }
 
 impl Attempt {
     /// Creates a Close attempt
     pub fn new(closer: Closer) -> Self {
         Self {
-            state: CloseState::SendCloseAttempt {
-                close_sender: closer.close_sender,
-                is_open: closer.is_open,
-            },
+            request_sent: false,
+            close_sender: closer.close_sender,
+            is_open: closer.is_open,
         }
     }
 }
@@ -47,70 +35,41 @@ impl Future for Attempt {
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Self::Output> {
-        loop {
-            match core::mem::replace(&mut self.state, CloseState::Unreachable) {
-                CloseState::SendCloseAttempt {
-                    mut close_sender,
-                    is_open,
-                } => {
-                    if is_open.load(Ordering::SeqCst) {
-                        let waker = cx.waker().clone();
-                        match close_sender.poll_ready(cx) {
-                            Poll::Ready(Ok(())) => {
-                                // send a waker to the endpoint, which is woken once the endpoint has closed
-                                match close_sender.try_send(waker) {
-                                    Ok(_) => {
-                                        self.state = CloseState::AwaitEndpointClose { is_open };
-                                    }
-                                    Err(err) if err.is_full() => {
-                                        // reset to the original state
-                                        self.state = CloseState::SendCloseAttempt {
-                                            close_sender,
-                                            is_open,
-                                        };
+        if !self.is_open.load(Ordering::SeqCst) {
+            return Poll::Ready(Ok(()));
+        }
 
-                                        // yield and wake up the task since the opener mis-reported its ready state
-                                        cx.waker().wake_by_ref();
-                                    }
-                                    Err(_) => {
-                                        // the endpoint is closed so return
-                                        return Poll::Ready(Ok(()));
-                                    }
-                                }
+        if !self.request_sent {
+            loop {
+                match self.close_sender.poll_ready(cx) {
+                    Poll::Ready(Ok(())) => {
+                        // send a waker to the endpoint, which is woken once the endpoint has closed
+                        match self.close_sender.try_send(cx.waker().clone()) {
+                            Ok(_) => {
+                                self.request_sent = true;
+                                return Poll::Pending;
                             }
-                            Poll::Ready(Err(_)) => {
+                            Err(err) if err.is_full() => {
+                                // yield and wake up the task since the opener mis-reported its ready state
+                                cx.waker().wake_by_ref();
+                            }
+                            Err(_) => {
                                 // the endpoint is closed so return
                                 return Poll::Ready(Ok(()));
                             }
-                            Poll::Pending => {
-                                // reset to the original state
-                                self.state = CloseState::SendCloseAttempt {
-                                    close_sender,
-                                    is_open,
-                                };
-                            }
                         }
-                    } else {
-                        // the endpoint is already closed so return
+                    }
+                    Poll::Ready(Err(_)) => {
+                        // the endpoint is closed so return
                         return Poll::Ready(Ok(()));
                     }
-
-                    return Poll::Pending;
-                }
-                CloseState::AwaitEndpointClose { is_open } => {
-                    if is_open.load(Ordering::SeqCst) {
-                        self.state = CloseState::AwaitEndpointClose { is_open };
-                        return Poll::Pending;
-                    } else {
-                        return Poll::Ready(Ok(()));
+                    Poll::Pending => {
+                        // pending capacity so try again
                     }
-                }
-                CloseState::Unreachable => {
-                    unreachable!(
-                        "Unreachable is an immediate state and should not exist across polls"
-                    );
                 }
             }
         }
+
+        Poll::Pending
     }
 }
