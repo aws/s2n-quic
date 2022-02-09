9ac3cde632ddd83ca14b11a9eccd414007997e2e
WesleyRosenblum <wesleyr@amazon.com>
2022-02-09 11:34:37 -0800

    Remove with_congestion_controller from Builders and make CongestionController non-public

--- quic/s2n-quic/src/server/builder.rs
+++ quic/s2n-quic/src/server/builder.rs
@@ -18,31 +18,6 @@ impl Default for Builder<DefaultProviders> {
 
 impl<Providers: ServerProviders> Builder<Providers> {
     impl_provider_method!(
-        /// Sets the congestion controller provider for the [`Server`]
-        ///
-        /// # Examples
-        ///
-        /// Sets the congestion controller to `Cubic` with the default configuration.
-        ///
-        /// ```rust,no_run
-        /// # use std::error::Error;
-        /// use s2n_quic::{Server, provider::congestion_controller};
-        ///
-        /// # #[tokio::main]
-        /// # async fn main() -> Result<(), Box<dyn Error>> {
-        /// let server = Server::builder()
-        ///     .with_congestion_controller(congestion_controller::cubic::Provider::default())?
-        ///     .start()?;
-        /// #
-        /// #    Ok(())
-        /// # }
-        /// ```
-        with_congestion_controller,
-        congestion_controller,
-        ServerProviders
-    );
-
-    impl_provider_method!(
         /// Sets the connection ID provider for the [`Server`]
         ///
         /// # Examples
