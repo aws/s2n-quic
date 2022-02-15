a956665659e6cc6de2fddba2b3e31629575b3043
WesleyRosenblum <wesleyr@amazon.com>
2022-02-14 17:36:35 -0800

    Use doc comments

--- examples/dos-mitigation/src/lib.rs
+++ examples/dos-mitigation/src/lib.rs
@@ -1,8 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Example mitigation for Slowloris-style Denial of Service attacks. For details on this attack,
-// see [QUIC Transport RFC](https://www.rfc-editor.org/rfc/rfc9000.html#name-slowloris-attacks).
+/// Example mitigation for Slowloris-style Denial of Service attacks. For details on this attack,
+/// see [QUIC Transport RFC](https://www.rfc-editor.org/rfc/rfc9000.html#name-slowloris-attacks).
 pub mod example {
     use s2n_quic::provider::{
         event,
@@ -10,34 +10,34 @@ pub mod example {
     };
     use std::time::Duration;
 
-    // The maximum number of connections that may be active concurrently before
-    // low throughput connections are closed.
+    /// The maximum number of connections that may be active concurrently before
+    /// low throughput connections are closed.
     const CONNECTION_COUNT_THRESHOLD: usize = 1000;
-    // The minimum throughput a connection must sustain, in bytes per second
+    /// The minimum throughput a connection must sustain, in bytes per second
     const MIN_THROUGHPUT: usize = 500;
 
-    // Define a Connection Context containing any per-connection state you wish to track.
-    // For this example, we need to track the number of bytes transferred and the last
-    // time the transferred byte count was totalled.
+    /// Define a Connection Context containing any per-connection state you wish to track.
+    /// For this example, we need to track the number of bytes transferred and the last
+    /// time the transferred byte count was totalled.
     #[derive(Debug, Clone)]
     pub struct MyConnectionContext {
         transferred_bytes: usize,
         last_update: Timestamp,
     }
 
-    // Define a struct containing any state across all connections you wish to track.
-    // For this example, there is no additional state we need to track so the struct is empty.
+    /// Define a struct containing any state across all connections you wish to track.
+    /// For this example, there is no additional state we need to track so the struct is empty.
     #[derive(Default)]
     pub struct MyConnectionSupervisor;
 
-    // Implement the `event::Subscriber` trait for your struct. The `create_connection_context`
-    // method must be implemented to initialize the Connection Context for each connection.
-    // Other methods may be implemented as needed.
+    /// Implement the `event::Subscriber` trait for your struct. The `create_connection_context`
+    /// method must be implemented to initialize the Connection Context for each connection.
+    /// Other methods may be implemented as needed.
     impl event::Subscriber for MyConnectionSupervisor {
         type ConnectionContext = MyConnectionContext;
 
-        // Initialize the Connection Context that is passed to the `supervisor_timeout` and
-        // `on_supervisor_timeout` methods, as well as each connection-related event.
+        /// Initialize the Connection Context that is passed to the `supervisor_timeout` and
+        /// `on_supervisor_timeout` methods, as well as each connection-related event.
         fn create_connection_context(
             &mut self,
             meta: &events::ConnectionMeta,
@@ -49,9 +49,9 @@ pub mod example {
             }
         }
 
-        // Implement `supervisor_timeout` to define the period at which `on_supervisor_timeout` will
-        // be invoked. For this example, a constant of 1 second is used, but this value can be
-        // varied over time or based on the connection.
+        /// Implement `supervisor_timeout` to define the period at which `on_supervisor_timeout` will
+        /// be invoked. For this example, a constant of 1 second is used, but this value can be
+        /// varied over time or based on the connection.
         fn supervisor_timeout(
             &mut self,
             _conn_context: &mut Self::ConnectionContext,
@@ -61,11 +61,11 @@ pub mod example {
             Some(Duration::from_secs(1))
         }
 
-        // Implement `on_supervisor_timeout` to define what action should be taken on the connection
-        // when the `supervisor_timeout` expires. For this example, the connection will be closed
-        // immediately (`supervisor::Outcome::ImmediateClose`) if the number of open connections
-        // is greater than `CONNECTION_COUNT_THRESHOLD` and the the throughput of the connection
-        // since the last `supervisor_timeout` has dropped below `MIN_THROUGHPUT`.
+        /// Implement `on_supervisor_timeout` to define what action should be taken on the connection
+        /// when the `supervisor_timeout` expires. For this example, the connection will be closed
+        /// immediately (`supervisor::Outcome::ImmediateClose`) if the number of open connections
+        /// is greater than `CONNECTION_COUNT_THRESHOLD` and the the throughput of the connection
+        /// since the last `supervisor_timeout` has dropped below `MIN_THROUGHPUT`.
         fn on_supervisor_timeout(
             &mut self,
             conn_context: &mut Self::ConnectionContext,
@@ -96,8 +96,8 @@ pub mod example {
             supervisor::Outcome::Continue
         }
 
-        // Implement `on_tx_stream_progress` to be notified every time forward progress is made
-        // on an outgoing stream.
+        /// Implement `on_tx_stream_progress` to be notified every time forward progress is made
+        /// on an outgoing stream.
         fn on_tx_stream_progress(
             &mut self,
             context: &mut Self::ConnectionContext,
@@ -107,8 +107,8 @@ pub mod example {
             context.transferred_bytes += event.bytes;
         }
 
-        // Implement `on_rx_progress` to be notified every time forward progress is made on an
-        // incoming stream.
+        /// Implement `on_rx_progress` to be notified every time forward progress is made on an
+        /// incoming stream.
         fn on_rx_stream_progress(
             &mut self,
             context: &mut Self::ConnectionContext,
