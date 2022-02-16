ccee8e329d9d237853854a3da876a04d990151dc
WesleyRosenblum <wesleyr@amazon.com>
2022-02-15 16:24:42 -0800

    Add connection docs

--- quic/s2n-quic/src/connection/acceptor.rs
+++ quic/s2n-quic/src/connection/acceptor.rs
@@ -8,12 +8,25 @@ use crate::{
 
 macro_rules! impl_accept_api {
     () => {
-        /// TODO
+        /// Accepts an incoming [`PeerStream`](`crate::stream::PeerStream`)
+        ///
+        /// The method will return
+        /// - `Ok(Some(stream)))` if a [`PeerStream`](`crate::stream::PeerStream`) was accepted
+        /// - `Ok(None)` if the connection was closed without an error
+        /// - `Err(stream_error)` if no stream could be accepted due to an error
         ///
         /// # Examples
         ///
-        /// ```rust
-        /// // TODO
+        /// ```rust,no_run
+        /// # async fn test() -> s2n_quic::stream::Result<()> {
+        /// #   let mut acceptor: s2n_quic::connection::StreamAcceptor = todo!();
+        /// #
+        /// while let Some(stream) = acceptor.accept()?.await {
+        ///     println!("Stream opened from {:?}", stream.connection().remote_addr());
+        /// }
+        /// #
+        /// #   Ok(())
+        /// # }
         /// ```
         #[inline]
         pub async fn accept(
@@ -22,13 +35,17 @@ macro_rules! impl_accept_api {
             futures::future::poll_fn(|cx| self.poll_accept(cx)).await
         }
 
-        /// TODO
+        /// Poll for accepting an incoming [`PeerStream`](`crate::stream::PeerStream`)
         ///
-        /// # Examples
-        ///
-        /// ```rust
-        /// // TODO
-        /// ```
+        /// The method will return
+        /// - `Poll::Ready(Ok(Some(stream)))` if a [`PeerStream`](`crate::stream::PeerStream`) was accepted
+        /// - `Poll::Ready(Ok(None))` if the connection was closed without an error
+        /// - `Poll::Ready(Err(stream_error))` if no stream could be accepted due to an error
+        /// - `Poll::Pending` if no new [`PeerStream`](`crate::stream::PeerStream`) was accepted by the connection yet.
+        ///   In this case the caller must retry calling [`Self::poll_accept`].
+        ///   For this purpose the method will save the [`core::task::Waker`]
+        ///   which is provided as part of the [`core::task::Context`] parameter, and notify it
+        ///   as soon as retrying the method will yield a different result.
         #[inline]
         pub fn poll_accept(
             &mut self,
@@ -55,12 +72,25 @@ macro_rules! impl_accept_api {
 
 macro_rules! impl_accept_bidirectional_api {
     () => {
-        /// TODO
+        /// Accepts an incoming [`BidirectionalStream`](`crate::stream::BidirectionalStream`)
+        ///
+        /// The method will return
+        /// - `Ok(Some(stream)))` if a [`BidirectionalStream`](`crate::stream::BidirectionalStream`) was accepted
+        /// - `Ok(None)` if the connection was closed without an error
+        /// - `Err(stream_error)` if no stream could be accepted due to an error
         ///
         /// # Examples
         ///
-        /// ```rust
-        /// // TODO
+        /// ```rust,no_run
+        /// # async fn test() -> s2n_quic::stream::Result<()> {
+        /// #   let mut acceptor: s2n_quic::connection::StreamAcceptor = todo!();
+        /// #
+        /// while let Ok(Some(mut stream)) = acceptor.accept_bidirectional_stream().await {
+        ///     println!("Stream opened from {:?}", stream.connection().remote_addr());
+        /// }
+        /// #
+        /// #   Ok(())
+        /// # }
         /// ```
         #[inline]
         pub async fn accept_bidirectional_stream(
@@ -69,13 +99,17 @@ macro_rules! impl_accept_bidirectional_api {
             futures::future::poll_fn(|cx| self.poll_accept_bidirectional_stream(cx)).await
         }
 
-        /// TODO
+        /// Poll for accepting an incoming [`BidirectionalStream`](`crate::stream::BidirectionalStream`)
         ///
-        /// # Examples
-        ///
-        /// ```rust
-        /// // TODO
-        /// ```
+        /// The method will return
+        /// - `Poll::Ready(Ok(Some(stream)))` if a [`BidirectionalStream`](`crate::stream::BidirectionalStream`) was accepted
+        /// - `Poll::Ready(Ok(None))` if the connection was closed without an error
+        /// - `Poll::Ready(Err(stream_error))` if no stream could be accepted due to an error
+        /// - `Poll::Pending` if no new [`BidirectionalStream`](`crate::stream::BidirectionalStream`) was accepted by the connection yet.
+        ///   In this case the caller must retry calling [`Self::poll_accept_bidirectional_stream`].
+        ///   For this purpose the method will save the [`core::task::Waker`]
+        ///   which is provided as part of the [`core::task::Context`] parameter, and notify it
+        ///   as soon as retrying the method will yield a different result.
         #[inline]
         pub fn poll_accept_bidirectional_stream(
             &mut self,
@@ -93,12 +127,25 @@ macro_rules! impl_accept_bidirectional_api {
 
 macro_rules! impl_accept_receive_api {
     () => {
-        /// TODO
+        /// Accepts an incoming [`ReceiveStream`](`crate::stream::ReceiveStream`)
+        ///
+        /// The method will return
+        /// - `Ok(Some(stream)))` if a [`ReceiveStream`](`crate::stream::ReceiveStream`) was accepted
+        /// - `Ok(None)` if the connection was closed without an error
+        /// - `Err(stream_error)` if no stream could be accepted due to an error
         ///
         /// # Examples
         ///
-        /// ```rust
-        /// // TODO
+        /// ```rust,no_run
+        /// # async fn test() -> s2n_quic::stream::Result<()> {
+        /// #   let mut acceptor: s2n_quic::connection::StreamAcceptor = todo!();
+        /// #
+        /// while let Ok(Some(mut stream)) = acceptor.accept_receive_stream().await {
+        ///     println!("Stream opened from {:?}", stream.connection().remote_addr());
+        /// }
+        /// #
+        /// #   Ok(())
+        /// # }
         /// ```
         #[inline]
         pub async fn accept_receive_stream(
@@ -107,13 +154,17 @@ macro_rules! impl_accept_receive_api {
             futures::future::poll_fn(|cx| self.poll_accept_receive_stream(cx)).await
         }
 
-        /// TODO
+        /// Poll for accepting an incoming [`ReceiveStream`](`crate::stream::ReceiveStream`)
         ///
-        /// # Examples
-        ///
-        /// ```rust
-        /// // TODO
-        /// ```
+        /// The method will return
+        /// - `Poll::Ready(Ok(Some(stream)))` if a [`ReceiveStream`](`crate::stream::ReceiveStream`) was accepted
+        /// - `Poll::Ready(Ok(None))` if the connection was closed without an error
+        /// - `Poll::Ready(Err(stream_error))` if no stream could be accepted due to an error
+        /// - `Poll::Pending` if no new [`ReceiveStream`](`crate::stream::ReceiveStream`) was accepted by the connection yet.
+        ///   In this case the caller must retry calling [`Self::poll_accept_receive_stream`].
+        ///   For this purpose the method will save the [`core::task::Waker`]
+        ///   which is provided as part of the [`core::task::Context`] parameter, and notify it
+        ///   as soon as retrying the method will yield a different result.
         #[inline]
         pub fn poll_accept_receive_stream(
             &mut self,
@@ -134,12 +185,32 @@ pub struct StreamAcceptor(pub(crate) s2n_quic_transport::connection::Connection)
 impl StreamAcceptor {
     impl_accept_api!();
 
-    /// TODO
+    /// Splits the [`StreamAcceptor`] into [`BidirectionalStreamAcceptor`] and [`ReceiveStreamAcceptor`] halves
     ///
     /// # Examples
     ///
-    /// ```rust
-    /// // TODO
+    /// ```rust,no_run
+    /// # use bytes::Bytes;
+    /// # async fn test() -> s2n_quic::stream::Result<()> {
+    /// #   let connection: s2n_quic::connection::Connection = todo!();
+    /// #
+    /// let (handle, acceptor) = new_conn.split();
+    /// let (bidi, recv) = acceptor.split();
+    ///
+    /// tokio::spawn(async move {
+    ///     while let Ok(Some(mut stream)) = bidi.accept_bidirectional_stream().await {
+    ///       println!("Bidirectional stream opened from {:?}", stream.connection().remote_addr());
+    ///     }
+    /// });
+    ///
+    /// tokio::spawn(async move {
+    ///     while let Ok(Some(mut stream)) = recv.accept_receive_stream().await {
+    ///       println!("Receive stream opened from {:?}", stream.connection().remote_addr());
+    ///     }
+    /// });
+    /// #
+    /// #   Ok(())
+    /// # }
     /// ```
     #[inline]
     pub fn split(self) -> (BidirectionalStreamAcceptor, ReceiveStreamAcceptor) {
