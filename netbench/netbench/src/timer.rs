87b3411f0fabc7741a4390bc361cf222374f9427
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-02-28 15:57:37 -0700

    netbench: add tokio timer

--- netbench/netbench/src/timer.rs
+++ netbench/netbench/src/timer.rs
@@ -1,10 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use core::{
-    task::{Context, Poll, Waker},
-    time::Duration,
-};
+use core::task::{Context, Poll};
 pub use s2n_quic_core::time::Timestamp;
 
 pub trait Timer {
@@ -12,61 +9,10 @@ pub trait Timer {
     fn poll(&mut self, target: Timestamp, cx: &mut Context) -> Poll<()>;
 }
 
-#[derive(Debug)]
-pub struct Testing {
-    now: Timestamp,
-    target: Option<Timestamp>,
-    waker: Option<Waker>,
-}
+#[cfg(any(test, feature = "testing"))]
+mod testing;
+mod tokio;
 
-impl Default for Testing {
-    fn default() -> Self {
-        Self {
-            now: unsafe { Timestamp::from_duration(Duration::ZERO) },
-            target: None,
-            waker: None,
-        }
-    }
-}
-
-impl Testing {
-    pub fn advance_pair(&mut self, other: &mut Self) -> Option<Timestamp> {
-        let target = match (self.target, other.target) {
-            (Some(a), Some(b)) => Some(a.min(b)),
-            (a, None) => a,
-            (None, b) => b,
-        };
-
-        if let Some(target) = target {
-            self.set(target);
-            other.set(target);
-            Some(self.now)
-        } else {
-            None
-        }
-    }
-
-    fn set(&mut self, target: Timestamp) {
-        self.now = target;
-        self.target = None;
-        if let Some(waker) = self.waker.take() {
-            waker.wake();
-        }
-    }
-}
-
-impl Timer for Testing {
-    fn now(&self) -> s2n_quic_core::time::Timestamp {
-        self.now
-    }
-
-    fn poll(&mut self, target: Timestamp, cx: &mut Context<'_>) -> Poll<()> {
-        if target == self.now {
-            Poll::Ready(())
-        } else {
-            self.target = Some(target);
-            self.waker = Some(cx.waker().clone());
-            Poll::Pending
-        }
-    }
-}
+pub use self::tokio::Timer as Tokio;
+#[cfg(any(test, feature = "testing"))]
+pub use testing::Timer as Testing;
