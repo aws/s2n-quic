92d2d0e4c25d8b2773e1e4ba997dfd370b476fa6
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-03-10 16:38:59 -0700

    chore(s2n-quic-integration): remove s2n-quic-integration crate

--- quic/s2n-quic-integration/src/rt.rs
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// TODO abstract over the current runtime
-pub use tokio::spawn;
-
-pub use tokio::time::sleep as delay;
