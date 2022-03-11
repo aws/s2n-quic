3a4a025a75363eed08f6dbfe2d17752044bee439
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-03-10 17:40:26 -0700

    chore(s2n-quic-integration): remove s2n-quic-integration crate (#1218)

--- quic/s2n-quic-integration/src/stream.rs
+++
@@ -1,9 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-mod endpoint;
-mod scenario;
-
-pub use endpoint::*;
-pub use s2n_quic_core::stream::testing::*;
-pub use scenario::*;
