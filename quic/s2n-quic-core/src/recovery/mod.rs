c12da0def5791ec3c389c5d111e23ab0d8df3bf1
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2022-03-30 12:00:30 -0700

    feat(s2n-quic-core): bandwidth estimator (#1244)
    * feat(s2n-quic-core): bandwidth estimator
    
    * Add bandwidth estimator to the path
    
    * Integrate BW estimator with recovery manager and add additional attributes to sent packet info
    
    * Add on_packet_sent tests
    
    * Remove eprintln
    
    * Add on_packet_loss test
    
    * Add citations
    
    * Fix merge issues
    
    * Pass sent packet info to bw estimator
    
    * Add additional unit tests
    
    * Reorder app_limited_timestamp
    
    * Break out bandwidth state into its own struct
    
    * Move bandwidth into its own mod
    
    * Remove getters
    
    * Use bandwidth::State directly in SentPacketInfo
    
    * Clippy fixes
    
    * Make SentPacketInfo generic over packet_info defined in the congestion controller
    
    * Rename some congestion controller methods and pass packet info to on packet lost
    
    * Rename on_packet_ack in bw estimator
    
    * Fix merge issue
    
    * Remove newly_acked, newly_lost and app_limited_timestamp
    
    * Fix comments

--- quic/s2n-quic-core/src/recovery/mod.rs
+++ quic/s2n-quic-core/src/recovery/mod.rs
@@ -6,6 +6,7 @@ pub use cubic::CubicCongestionController;
 pub use rtt_estimator::*;
 pub use sent_packets::*;
 
+pub mod bandwidth;
 pub mod congestion_controller;
 pub mod cubic;
 mod hybrid_slow_start;
