72301b2f94b7321f25dd858d141b6806b4a528be
Wesley Rosenblum <wesleyr@amazon.com>
2022-04-22 19:21:44 -0700

    Add MinRttWindowedFilter

--- quic/s2n-quic-core/src/recovery/bbr/windowed_filter.rs
+++ quic/s2n-quic-core/src/recovery/bbr/windowed_filter.rs
@@ -1,7 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use core::marker::PhantomData;
+use crate::time::Timestamp;
+use core::{marker::PhantomData, time::Duration};
 
 /// Data structure for tracking the minimum or maximum value seen over a configurable
 /// time period specified by the `window_length`
@@ -73,11 +74,7 @@ impl<
     /// `now` must be monotonically increasing, unless the `TimeType` supports wrapping (such as
     /// `core::num::Wrapping`)
     pub fn update(&mut self, new_sample: T, now: TimeType) {
-        let current_value_expired = self.last_updated.map_or(true, |last_updated| {
-            now - last_updated >= self.window_length
-        });
-
-        if current_value_expired || FilterType::supersedes(new_sample, self.current_value) {
+        if self.window_expired(now) || FilterType::supersedes(new_sample, self.current_value) {
             self.current_value = Some(new_sample);
             self.last_updated = Some(now);
         }
@@ -86,6 +83,89 @@ impl<
     /// Returns the current value if one has been recorded yet
     pub fn value(&self) -> Option<T> {
         self.current_value
+    }
+
+    #[inline]
+    fn window_expired(&self, now: TimeType) -> bool {
+        self.last_updated.map_or(false, |last_updated| {
+            now - last_updated >= self.window_length
+        })
+    }
+}
+
+/// Specialized WindowedMinFilter for tracking min round trip time
+///
+/// BBRv2 tracks both a min probe RTT that is refreshed at least every `PROBE_RTT_INTERVAL`,
+/// and an overall min_rtt that is refreshed at least every `MIN_RTT_FILTER_LEN`.
+pub(crate) struct MinRttWindowedFilter {
+    min_probe_rtt: WindowedFilter<Duration, Timestamp, Duration, MinFilter>,
+    min_rtt: WindowedFilter<Duration, Timestamp, Duration, MinFilter>,
+    //= https://tools.ietf.org/id/draft-cardwell-iccrg-bbr-congestion-control-02#2.14.2
+    //# A boolean recording whether the BBR.probe_rtt_min_delay has expired and is due for a
+    //# refresh with an application idle period or a transition into ProbeRTT state.
+    probe_rtt_expired: bool,
+}
+
+//= https://tools.ietf.org/id/draft-cardwell-iccrg-bbr-congestion-control-02#2.14.2
+//# A constant specifying the minimum time interval between ProbeRTT states: 5 secs.
+const PROBE_RTT_INTERVAL: Duration = Duration::from_secs(5);
+
+//= https://tools.ietf.org/id/draft-cardwell-iccrg-bbr-congestion-control-02#2.14.1
+//# A constant specifying the length of the BBR.min_rtt min filter window,
+//# MinRTTFilterLen is 10 secs.
+const MIN_RTT_FILTER_LEN: Duration = Duration::from_secs(10);
+
+impl MinRttWindowedFilter {
+    /// Constructs a new MinRttWindowedFilter
+    pub fn new() -> Self {
+        Self {
+            min_probe_rtt: WindowedMinFilter::new(PROBE_RTT_INTERVAL),
+            min_rtt: WindowedMinFilter::new(MIN_RTT_FILTER_LEN),
+            probe_rtt_expired: false,
+        }
+    }
+
+    /// Updates the min_probe_rtt and min_rtt estimates with the given `rtt`
+    pub fn update(&mut self, rtt: Duration, now: Timestamp) {
+        self.probe_rtt_expired = self.min_probe_rtt.window_expired(now);
+        self.min_probe_rtt.update(rtt, now);
+
+        let probe_rtt = self
+            .min_probe_rtt
+            .value()
+            .expect("probe_rtt is updated just prior");
+
+        if self.min_rtt.window_expired(now)
+            || MinFilter::supersedes(probe_rtt, self.min_rtt.value())
+        {
+            // When the min_rtt expires or is superseded, it is replaced with the
+            // min_probe_rtt value and the last updated time from min_probe_rtt
+            // rather than the latest RTT and current time to keep min_probe_rtt and
+            // min_rtt coordinated.
+            self.min_rtt.current_value = self.min_probe_rtt.value();
+            self.min_rtt.last_updated = self.min_probe_rtt.last_updated;
+        }
+    }
+
+    /// The current windowed estimate of minimum round trip time
+    #[allow(dead_code)] // TODO: Remove when used
+    pub fn min_rtt(&self) -> Option<Duration> {
+        self.min_rtt.value()
+    }
+
+    /// True if the probe RTT has expired and is due for a refresh by entering the ProbeRTT state
+    #[allow(dead_code)] // TODO: Remove when used
+    pub fn probe_rtt_expired(&self) -> bool {
+        self.probe_rtt_expired
+    }
+
+    /// Overrides the last updated time for Min Probe RTT to ensure ProbeRTT is not entered until
+    /// the next `PROBE_RTT_INTERVAL`.
+    ///
+    /// Called immediately after ProbeRTT period ends or after resuming from idle
+    #[allow(dead_code)] // TODO: Remove when used
+    pub fn schedule_next_probe_rtt(&mut self, now: Timestamp) {
+        self.min_probe_rtt.last_updated = Some(now);
     }
 }
 
@@ -192,5 +272,53 @@ mod tests {
         filter.update(1, now);
         assert_eq!(Some(1), filter.value());
         assert_eq!(Some(now), filter.last_updated);
+    }
+
+    #[test]
+    fn min_rtt_filter() {
+        let mut filter = MinRttWindowedFilter::new();
+
+        // Filter has not received an update, so no value should be present
+        assert_eq!(None, filter.min_rtt());
+        // Probe RTT is not expired upon initialization
+        assert!(!filter.probe_rtt_expired());
+
+        // After the first update, the first value is the min
+        let now = NoopClock.get_time();
+        filter.update(Duration::from_millis(5), now);
+        assert_eq!(Some(Duration::from_millis(5)), filter.min_rtt());
+        assert!(!filter.probe_rtt_expired());
+
+        // PROBE_RTT_INTERVAL has passed
+        let now = now + PROBE_RTT_INTERVAL;
+        filter.update(Duration::from_millis(9), now);
+        // min_rtt is not updated since it has not expired and is lower than the new rtt
+        assert_eq!(Some(Duration::from_millis(5)), filter.min_rtt());
+        assert!(filter.probe_rtt_expired());
+
+        // Midway through the Probe RTT period the RTT drops
+        let now = now + Duration::from_secs(2);
+        let probe_rtt_update_time = now;
+        filter.update(Duration::from_millis(7), now);
+        // min_rtt is not updated since it has not expired and is lower than the new rtt
+        assert_eq!(Some(Duration::from_millis(5)), filter.min_rtt());
+        assert!(!filter.probe_rtt_expired());
+        assert_eq!(Some(Duration::from_millis(7)), filter.min_probe_rtt.value());
+
+        // Now the Min RTT has expired
+        let now = now + Duration::from_secs(3);
+        filter.update(Duration::from_millis(10), now);
+        // min_rtt is updated since it has expired. The value is set to the current probe_rtt
+        assert_eq!(Some(Duration::from_millis(7)), filter.min_rtt());
+        // min_rtt last_updated is set to the probe_rtt last updated time
+        assert_eq!(Some(probe_rtt_update_time), filter.min_rtt.last_updated);
+        assert!(!filter.probe_rtt_expired());
+
+        filter.schedule_next_probe_rtt(now);
+
+        let now = now + PROBE_RTT_INTERVAL;
+        assert!(!filter.probe_rtt_expired());
+        filter.update(Duration::from_secs(10), now);
+        assert!(filter.probe_rtt_expired());
     }
 }
