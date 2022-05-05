7c4fbd7fb01a25c0024ca07e19398cc98a93ed40
carl Lundin <lundinc@amazon.com>
2022-05-05 11:08:33 -0700

    Why is my formatter missing this?

--- quic/s2n-quic-core/src/endpoint/limits.rs
+++ quic/s2n-quic-core/src/endpoint/limits.rs
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    event::Timestamp,
-    event::{api::SocketAddress, IntoEvent},
+    event::{api::SocketAddress, IntoEvent, Timestamp},
     inet,
 };
 
