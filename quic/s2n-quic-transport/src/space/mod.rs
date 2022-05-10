d29d30c4ce27f416199b333375ab7a11bca7b7bd
maddeleine <59030281+maddeleine@users.noreply.github.com>
2022-05-10 09:27:15 -0700

    Receiver trait (#1304)

--- quic/s2n-quic-transport/src/space/mod.rs
+++ quic/s2n-quic-transport/src/space/mod.rs
@@ -663,11 +663,7 @@ pub trait PacketSpace<Config: endpoint::Config> {
             .with_frame_type(frame.tag().into()))
     }
 
-    fn handle_datagram_frame(
-        &mut self,
-        frame: DatagramRef,
-        _packet: &mut ProcessedPacket,
-    ) -> Result<(), transport::Error> {
+    fn handle_datagram_frame(&mut self, frame: DatagramRef) -> Result<(), transport::Error> {
         Err(transport::Error::PROTOCOL_VIOLATION
             .with_reason(Self::INVALID_FRAME_ERROR)
             .with_frame_type(frame.tag().into()))
@@ -810,8 +806,7 @@ pub trait PacketSpace<Config: endpoint::Config> {
                 }
                 Frame::Datagram(frame) => {
                     let on_error = on_frame_processed!(frame);
-                    self.handle_datagram_frame(frame.into(), &mut processed_packet)
-                        .map_err(on_error)?;
+                    self.handle_datagram_frame(frame.into()).map_err(on_error)?;
                 }
                 Frame::DataBlocked(frame) => {
                     let on_error = on_frame_processed!(frame);
