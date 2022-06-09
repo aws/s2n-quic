f854ecf3f9e71371c5d1a7bb1e0f57dde9f2b807
Ni <orrinni@bcd07416b4bf.ant.amazon.com>
2022-06-09 16:17:11 -0400

    changed directory name to cdk to avoid Cargo.toml errors

---
+++ netbench/cdk/src/main/java/com/aws/ClientStack.java
@@ -0,0 +1,27 @@
+package com.aws;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.GatewayVpcEndpoint;
+import software.amazon.awscdk.services.ec2.GatewayVpcEndpointOptions;
+import software.amazon.awscdk.services.ec2.GatewayVpcEndpointAwsService;
+
+
+public class ClientStack extends Stack {
+    public ClientStack(final Construct parent, final String id) {
+        this(parent, id, null, null, null);
+    }
+
+    public ClientStack(final Construct parent, final String id, Bucket metricsBucket,
+        String instanceType, final StackProps props) {
+        super(parent, id, props);
+
+    }
+}                                         
