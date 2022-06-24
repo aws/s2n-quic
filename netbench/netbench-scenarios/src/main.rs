3096e046adee004fe1cab835b4e0066977ab0c10
Cameron Bytheway <bythewc@amazon.com>
2022-06-24 10:10:08 -0600

    feat(netbench): add connect scenario

--- netbench/netbench-scenarios/src/main.rs
+++ netbench/netbench-scenarios/src/main.rs
@@ -1,4 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-netbench_scenarios::scenarios!(request_response);
+netbench_scenarios::scenarios!(connect, request_response);
