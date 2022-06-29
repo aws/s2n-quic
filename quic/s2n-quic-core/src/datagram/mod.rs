b8b9a5c682575b16d9fe5ae75fc00ab2d28dd895
Wesley Rosenblum <wesleyr@amazon.com>
2022-06-29 10:53:41 -0700

    fix(s2n-quic-core): gate default datagram impl and fix no_std check

--- quic/s2n-quic-core/src/datagram/mod.rs
+++ quic/s2n-quic-core/src/datagram/mod.rs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "alloc", feature = "std"))]
 pub mod default;
 pub mod disabled;
 pub mod traits;
