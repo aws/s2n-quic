38409d931165cf36656c06b5f7e5c2a0fcb46997
Ivan Pavluk <chayleaf@protonmail.com>
2022-08-27 10:46:34 +0700

    fix(s2n-quic-crypto): x86 compilation (#1472)

--- quic/s2n-quic-crypto/src/arch.rs
+++ quic/s2n-quic-crypto/src/arch.rs
@@ -5,7 +5,7 @@ use cfg_if::cfg_if;
 
 cfg_if! {
     if #[cfg(target_arch = "x86")] {
-        pub use core::arch::x86::::*;
+        pub use core::arch::x86::*;
         mod x86;
         pub use x86::*;
     } else if #[cfg(target_arch = "x86_64")] {
