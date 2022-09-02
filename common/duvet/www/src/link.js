048e61d85e677225e4178c82c9d1788fdd92d6c4
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-09-02 10:58:25 -0600

    ci: use published duvet

--- common/duvet/www/src/link.js
+++
@@ -1,8 +0,0 @@
-import { Link as A } from "react-router-dom";
-import B from "@material-ui/core/Link";
-
-export function Link(props) {
-  if (props.href) return <B {...props} />;
-
-  return <B component={A} {...props} />;
-}
