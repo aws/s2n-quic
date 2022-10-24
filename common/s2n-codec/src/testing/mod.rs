4caf765a8faf4b37dabef90e4a9e294803b33158
Cameron Bytheway <bythewc@amazon.com>
2022-10-24 12:05:48 -0600

    ci: update kani to 0.13 (#1549)

--- common/s2n-codec/src/testing/mod.rs
+++ common/s2n-codec/src/testing/mod.rs
@@ -6,14 +6,7 @@ pub use crate::{
     EncoderBuffer, EncoderLenEstimator, EncoderValue,
 };
 
-#[derive(Clone, Debug)]
-pub struct Error(String);
-
-impl From<DecoderError> for Error {
-    fn from(err: DecoderError) -> Self {
-        Self(err.to_string())
-    }
-}
+pub type Error = DecoderError;
 
 #[macro_export]
 macro_rules! assert_codec_round_trip_value {
@@ -154,9 +147,9 @@ macro_rules! assert_codec_round_trip_sample_file {
 }
 
 macro_rules! ensure {
-    ($expr:expr, $message:expr $(, $format:expr)*) => {
+    ($expr:expr, $message:expr $(,)?) => {
         if !($expr) {
-            return Err(Error(format!($message $(, $format)*)));
+            return Err($crate::testing::Error::InvariantViolation($message));
         }
     };
 }
@@ -191,9 +184,7 @@ pub fn ensure_decoding_matches<'a, T: DecoderValue<'a> + PartialEq + core::fmt::
     let (actual_value, remaining) = decode(expected_bytes)?;
     ensure!(
         expected_value == &actual_value,
-        "mut decodings do not match:\n expected: {:?}\n   actual: {:?}",
-        expected_value,
-        actual_value
+        "mut decodings do not match",
     );
     remaining.ensure_empty()?;
     Ok(())
@@ -213,9 +204,7 @@ pub fn ensure_decoding_mut_matches<'a, T: DecoderValueMut<'a> + PartialEq + core
     let (actual_value, remaining) = decode_mut(expected_bytes)?;
     ensure!(
         expected_value == &actual_value,
-        "mut decodings do not match:\n expected: {:?}\n   actual: {:?}",
-        expected_value,
-        actual_value
+        "mut decodings do not match",
     );
     remaining.ensure_empty()?;
     Ok(())
