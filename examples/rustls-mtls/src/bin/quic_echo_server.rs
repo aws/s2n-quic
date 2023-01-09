6ba3acc69cfa95352a5e562f288087b19d3dc4d6
Rick Richardson <ririchardson@tesla.com>
2023-01-09 14:13:19 -0800

    adding mtls example using custom tls::Provider

---
+++ examples/rustls-mtls/src/bin/quic_echo_server.rs
@@ -0,0 +1,41 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use rustls_mtls::MtlsProvider;
+use s2n_quic::Server;
+use std::error::Error;
+
+/// NOTE: this certificate is to be used for demonstration purposes only!
+pub static CACERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/ca.pem");
+pub static MY_CERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/server.pem");
+pub static MY_KEY_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/server-key.pem");
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let provider = MtlsProvider::new(CACERT_PEM, MY_CERT_PEM, MY_KEY_PEM)?;
+    let mut server = Server::builder()
+        .with_tls(provider)?
+        .with_io("127.0.0.1:4433")?
+        .start()?;
+
+    while let Some(mut connection) = server.accept().await {
+        // spawn a new task for the connection
+        tokio::spawn(async move {
+            eprintln!("Connection accepted from {:?}", connection.remote_addr());
+
+            while let Ok(Some(mut stream)) = connection.accept_bidirectional_stream().await {
+                // spawn a new task for the stream
+                tokio::spawn(async move {
+                    eprintln!("Stream opened from {:?}", stream.connection().remote_addr());
+
+                    // echo any data back to the stream
+                    while let Ok(Some(data)) = stream.receive().await {
+                        stream.send(data).await.expect("stream should be open");
+                    }
+                });
+            }
+        });
+    }
+
+    Ok(())
+}
---
+++ examples/rustls-mtls/src/lib.rs
@@ -0,0 +1,125 @@
+use rustls::{
+    cipher_suite as ciphers, quic, CipherSuite, ClientConfig, Error, RootCertStore, ServerConfig,
+    SupportedCipherSuite,
+};
+use s2n_quic::provider::tls;
+use s2n_quic_rustls::{Client, Server};
+use std::io::{BufReader, Cursor, Read};
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+static PROTOCOL_VERSIONS: &[&rustls::SupportedProtocolVersion] = &[&rustls::version::TLS13];
+
+pub static DEFAULT_CIPHERSUITES: &[SupportedCipherSuite] = &[
+    ciphers::TLS13_AES_128_GCM_SHA256,
+    ciphers::TLS13_AES_256_GCM_SHA384,
+    ciphers::TLS13_CHACHA20_POLY1305_SHA256,
+];
+
+pub struct MtlsProvider {
+    root_store: rustls::RootCertStore,
+    my_cert_chain: Vec<rustls::Certificate>,
+    my_private_key: rustls::PrivateKey,
+}
+
+impl tls::Provider for MtlsProvider {
+    type Server = Server;
+    type Client = Client;
+    type Error = rustls::Error;
+
+    fn start_server(self) -> Result<Self::Server, Self::Error> {
+        let verifier = rustls::server::AllowAnyAuthenticatedClient::new(self.root_store);
+        let mut cfg = ServerConfig::builder()
+            .with_cipher_suites(DEFAULT_CIPHERSUITES)
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(PROTOCOL_VERSIONS)?
+            .with_client_cert_verifier(verifier)
+            .with_single_cert(self.my_cert_chain, self.my_private_key)?;
+
+        cfg.ignore_client_order = true;
+        cfg.max_fragment_size = None;
+        cfg.alpn_protocols = vec![b"h3".to_vec()];
+        Ok(cfg.into())
+    }
+
+    fn start_client(self) -> Result<Self::Client, Self::Error> {
+        let mut cfg = ClientConfig::builder()
+            .with_cipher_suites(DEFAULT_CIPHERSUITES)
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(PROTOCOL_VERSIONS)?
+            .with_root_certificates(self.root_store)
+            .with_single_cert(self.my_cert_chain, self.my_private_key)?;
+
+        cfg.max_fragment_size = None;
+        cfg.alpn_protocols = vec![b"h3".to_vec()];
+        Ok(cfg.into())
+    }
+}
+
+impl MtlsProvider {
+    pub fn new<A: AsRef<Path>, B: AsRef<Path>, C: AsRef<Path>>(
+        ca_cert_pem: A,
+        my_cert_pem: B,
+        my_key_pem: C,
+    ) -> Result<Self, Error> {
+        let root_store = into_root_store(ca_cert_pem.as_ref())?;
+        let cert_chain = into_certificate(my_cert_pem.as_ref())?;
+        let private_key = into_private_key(my_key_pem.as_ref())?;
+        Ok(MtlsProvider {
+            root_store,
+            my_cert_chain: cert_chain.into_iter().map(rustls::Certificate).collect(),
+            my_private_key: rustls::PrivateKey(private_key),
+        })
+    }
+}
+
+fn into_certificate(path: &Path) -> Result<Vec<Vec<u8>>, Error> {
+    let mut rdr = BufReader::new(
+        std::fs::File::open(path)
+            .map_err(|e| Error::General(format!("Failed to load file: {}", e)))?,
+    );
+    let certs = rustls_pemfile::certs(&mut rdr)
+        .map(|certs| certs.into_iter().collect())
+        .map_err(|_| Error::General("Could not read certificate".to_string()))?;
+    Ok(certs)
+}
+
+fn into_root_store(path: &Path) -> Result<RootCertStore, Error> {
+    let ca_certs = into_certificate(path)?;
+    let mut cert_store = RootCertStore::empty();
+    cert_store.add_parsable_certificates(ca_certs.as_slice());
+    Ok(cert_store)
+}
+
+fn into_private_key(path: &Path) -> Result<Vec<u8>, Error> {
+    let mut f = std::fs::File::open(path)
+        .map_err(|e| Error::General(format!("Failed to load file: {}", e)))?;
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf)
+        .map_err(|e| Error::General(format!("Failed to read file: {}", e)))?;
+    let mut cursor = Cursor::new(buf);
+
+    let parsers = [
+        rustls_pemfile::rsa_private_keys,
+        rustls_pemfile::pkcs8_private_keys,
+    ];
+    for parser in parsers.iter() {
+        cursor.set_position(0);
+
+        match parser(&mut cursor) {
+            Ok(keys) if keys.is_empty() => continue,
+            Ok(mut keys) if keys.len() == 1 => return Ok(rustls::PrivateKey(keys.pop().unwrap()).0),
+            Ok(keys) => {
+                return Err(Error::General(format!(
+                    "Unexpected number of keys: {} (only 1 supported)",
+                    keys.len()
+                )));
+            }
+            // try the next parser
+            Err(_) => continue,
+        }
+    }
+    Err(Error::General(
+        "could not load any valid private keys".to_string(),
+    ))
+}
