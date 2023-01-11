0c4a22260fb9400ea3a043c55fa142abec235bae
Rick Richardson <ririchardson@tesla.com>
2023-01-11 15:25:14 +0000

    adding copyright to example rustls-mtls/src/lib.rs

--- examples/rustls-mtls/src/lib.rs
+++ examples/rustls-mtls/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use rustls::{
     cipher_suite, ClientConfig, Error, RootCertStore, ServerConfig, SupportedCipherSuite,
 };
