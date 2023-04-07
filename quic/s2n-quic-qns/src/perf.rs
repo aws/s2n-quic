13aec7b48b3aebc474f8c8a4b24ba9a4796d8095
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2023-04-06 18:05:05 -0700

    feat(s2n-quic-qns): Add max pacing rate and delivery rate to perf stats (#1699)
    * feat(s2n-quic-qns): Add max pacing rate and delivery rate to perf stats
    
    * Added comments
    
    * Added header and formatted
    
    * Fix formatting

--- quic/s2n-quic-qns/src/perf.rs
+++ quic/s2n-quic-qns/src/perf.rs
@@ -143,6 +143,7 @@ pub struct Subscriber {
 impl Subscriber {
     pub fn spawn(&self, frequency: Duration) {
         let counters = self.counters.clone();
+        counters.print_header();
         tokio::spawn(async move {
             loop {
                 tokio::time::sleep(frequency).await;
@@ -243,6 +244,31 @@ impl s2n_quic::provider::event::Subscriber for Subscriber {
             Ordering::Relaxed,
         );
     }
+
+    #[inline]
+    fn on_pacing_rate_updated(
+        &mut self,
+        _context: &mut Self::ConnectionContext,
+        _meta: &event::events::ConnectionMeta,
+        event: &event::events::PacingRateUpdated,
+    ) {
+        self.counters
+            .max_pacing_rate
+            .fetch_max(event.bytes_per_second, Ordering::Relaxed);
+    }
+
+    #[inline]
+    fn on_delivery_rate_sampled(
+        &mut self,
+        _context: &mut Self::ConnectionContext,
+        _meta: &event::events::ConnectionMeta,
+        event: &event::events::DeliveryRateSampled,
+    ) {
+        self.counters.max_delivery_rate.fetch_max(
+            event.rate_sample.delivery_rate_bytes_per_second,
+            Ordering::Relaxed,
+        );
+    }
 }
 
 #[derive(Debug, Default)]
@@ -257,29 +283,75 @@ pub struct Counters {
     event_loop_wakeup: AtomicU64,
     timeout: AtomicU64,
     pto_count: AtomicU64,
+    max_pacing_rate: AtomicU64,
+    max_delivery_rate: AtomicU64,
 }
 
 impl Counters {
+    pub fn print_header(&self) {
+        eprintln!(
+            "Tx Rate\t\
+            Rx Rate\t\
+            Max Cwnd\t\
+            Max Inflight\t\
+            Lost Packets\t\
+            Wakeups\t\
+            Duration\t\
+            Max RTT\t\
+            Max SRTT\t\
+            PTO Count\t\
+            Max Pacing Rate\t\
+            Max Delivery Rate"
+        );
+    }
+
     pub fn print(&self, duration: Duration) {
+        // The goodput of data transmitted to the peer
         let send_progress = self.send_progress.swap(0, Ordering::Relaxed);
         let send_rate = rate(send_progress, duration);
+        // The goodput of data received from the peer
         let receive_progress = self.receive_progress.swap(0, Ordering::Relaxed);
         let receive_rate = rate(receive_progress, duration);
+        // The maximum congestion window observed during the interval
         let max_cwnd = self.max_cwnd.swap(0, Ordering::Relaxed);
         let max_cwnd = bytes(max_cwnd);
+        // The maximum amount of unacknowledged data in flight during the interval
         let max_bytes_in_flight = self.max_bytes_in_flight.swap(0, Ordering::Relaxed);
         let max_bytes_in_flight = bytes(max_bytes_in_flight);
+        // The maximum round trip time observed during the interval
         let max_rtt = self.max_rtt.swap(0, Ordering::Relaxed);
         let max_rtt = Duration::from_nanos(max_rtt);
+        // The maximum smoothed (weighted average) round trip time observed during the interval
         let max_smoothed_rtt = self.max_smoothed_rtt.swap(0, Ordering::Relaxed);
         let max_smoothed_rtt = Duration::from_nanos(max_smoothed_rtt);
+        // The number of packets recorded lost during the interval
         let lost_packets = self.lost_packets.swap(0, Ordering::Relaxed);
+        // The number of event loop wakeups recorded during the interval
         let wakeups = self.event_loop_wakeup.swap(0, Ordering::Relaxed);
+        // The duration of the latest event loop wakeup
         let duration = self.timeout.swap(0, Ordering::Relaxed);
         let duration = Duration::from_nanos(duration);
+        // The number of packet time out events observed during the interval
         let pto_count = self.pto_count.swap(0, Ordering::Relaxed);
+        // The maximum rate at which packets are paced out observed during the interval
+        let max_pacing_rate = self.max_pacing_rate.swap(0, Ordering::Relaxed);
+        let max_pacing_rate = rate(max_pacing_rate, Duration::from_secs(1));
+        // The maximum estimate of bandwidth observed during the interval. Only output for BBRv2
+        let max_delivery_rate = self.max_delivery_rate.swap(0, Ordering::Relaxed);
+        let max_delivery_rate = rate(max_delivery_rate, Duration::from_secs(1));
         eprintln!(
-            "{send_rate}\t{receive_rate}\t{max_cwnd}\t{max_bytes_in_flight}\t{lost_packets}\t{wakeups}\t{duration:?}\t{max_rtt:?}\t{max_smoothed_rtt:?}\t{pto_count}",
+            "{send_rate}\t\
+            {receive_rate}\t\
+            {max_cwnd}\t\
+            {max_bytes_in_flight}\t\
+            {lost_packets}\t\
+            {wakeups}\t\
+            {duration:?}\t\
+            {max_rtt:?}\t\
+            {max_smoothed_rtt:?}\t\
+            {pto_count}\t\
+            {max_pacing_rate}\t\
+            {max_delivery_rate}",
         );
     }
 }
