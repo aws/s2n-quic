279ea08bf15ac261d5f7d4fd707142f645156cdb
Carl Dong <accounts@carldong.me>
2023-06-27 22:11:18 -0400

    fix(s2n-quic-platform): Fix compilation in non-pktinfo platforms (#1849)

--- quic/s2n-quic-platform/src/message/cmsg.rs
+++ quic/s2n-quic-platform/src/message/cmsg.rs
@@ -42,7 +42,7 @@ pub const MAX_LEN: usize = {
     #[cfg(s2n_quic_platform_pktinfo)]
     let pktinfo_size = size_of_cmsg::<libc::in_pktinfo>() + size_of_cmsg::<libc::in6_pktinfo>();
     #[cfg(not(s2n_quic_platform_pktinfo))]
-    let pktinfo = 0;
+    let pktinfo_size = 0;
 
     // This is currently needed due to how we detect if CMSG data has been written or not.
     //
