73a814240c5db6fae261a6e4ab567b0b094a35db
Wesley Rosenblum <55108558+WesleyRosenblum@users.noreply.github.com>
2023-07-24 17:05:22 -0400

    Merge pull request from GHSA-rfhg-rjfp-9q8q
    * fix(s2n-quic-transport): close connection when no available connection ids
    
    * allow all connection migrations when testing
    
    * add generator to stateless reset token
    
    * fix partial eq impl for connection IDs
    
    * add new connection ID to fuzz test
    
    * implement hash and fix merge issues
    
    * Impl PartialOrd and Ord
    
    * PR feedback
    
    * clippy

--- quic/s2n-quic-transport/src/recovery/manager/tests.rs
+++ quic/s2n-quic-transport/src/recovery/manager/tests.rs
@@ -3152,7 +3152,7 @@ fn helper_generate_multi_path_manager(
                 &datagram,
                 true,
                 &mut Endpoint::default(),
-                &mut migration::default::Validator,
+                &mut migration::allow_all::Validator,
                 DEFAULT_MAX_MTU,
                 publisher,
             )
