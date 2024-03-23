0a60ec19ea9a8cd1bef98ad9798327521d2121ee
toidiu <apoorv@toidiu.com>
2024-03-22 18:38:09 -0700

    test: generate script for testing certs and keys (#2161)

--- quic/s2n-quic-core/certs/generate.sh
+++
@@ -1,9 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# immediately bail if any command fails
-set -e
-
-# generate PKCS#1 encoded RSA key (openSSL 1.1.1)
-echo "generating PKCS #1 encoded RSA key"
-openssl genrsa -f4 -out key_pkcs1.pem 2048
