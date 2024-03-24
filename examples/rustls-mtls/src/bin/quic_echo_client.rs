3dea51c50335c1b7045ffd4162f3ff664d7b593d
LAN Xingcan <thynson@live.com>
2024-03-24 22:21:30 +0800

    chore: update rustls-mtls example

--- examples/rustls-mtls/src/bin/quic_echo_client.rs
+++ examples/rustls-mtls/src/bin/quic_echo_client.rs
@@ -1,9 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use rustls_mtls::{initialize_logger, MtlsProvider};
+use rustls_mtls::{initialize_logger, into_certificate, into_private_key};
 use s2n_quic::{client::Connect, Client};
 use std::{error::Error, net::SocketAddr};
+use std::path::Path;
 
 /// NOTE: this certificate is to be used for demonstration purposes only!
 pub static CACERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/ca-cert.pem");
@@ -13,10 +14,13 @@ pub static MY_KEY_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/client
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     initialize_logger("client");
-    let provider = MtlsProvider::new(CACERT_PEM, MY_CERT_PEM, MY_KEY_PEM).await?;
     let client = Client::builder()
         .with_event(s2n_quic::provider::event::tracing::Subscriber::default())?
-        .with_tls(provider)?
+        .with_tls(s2n_quic_rustls::Client::builder()
+            .with_application_protocols(vec!["h3"].into_iter())?
+            .with_certificate(Path::new(CACERT_PEM))?
+            .with_client_identity(Path::new(MY_CERT_PEM), Path::new(MY_KEY_PEM))?
+            .build()?)?
         .with_io("0.0.0.0:0")?
         .start()?;
 
--- examples/rustls-mtls/src/lib.rs
+++ examples/rustls-mtls/src/lib.rs
@@ -36,64 +36,6 @@ pub fn initialize_logger(endpoint: &str) {
             .init();
     });
 }
-
-pub struct MtlsProvider {
-    root_store: RootCertStore,
-    my_cert_chain: Vec<CertificateDer<'static>>,
-    my_private_key: PrivateKeyDer<'static>,
-}
-
-impl s2n_quic_tls_provider::Provider for MtlsProvider {
-    type Server = s2n_quic_tls_provider::rustls::Server;
-    type Client = s2n_quic_tls_provider::rustls::Client;
-    type Error = RustlsError;
-
-    fn start_server(self) -> Result<Self::Server, Self::Error> {
-        let default_crypto_provider = s2n_quic_tls_provider::rustls::default_crypto_provider()?;
-        let verifier = WebPkiClientVerifier::builder_with_provider(
-            Arc::new(self.root_store),
-            default_crypto_provider.into(),
-        )
-        .build()
-        .unwrap();
-        let mut cfg = s2n_quic_tls_provider::rustls::server_config_builder()?
-            .with_client_cert_verifier(verifier)
-            .with_single_cert(self.my_cert_chain, self.my_private_key)?;
-
-        cfg.ignore_client_order = true;
-        cfg.max_fragment_size = None;
-        cfg.alpn_protocols = vec![b"h3".to_vec()];
-        Ok(cfg.into())
-    }
-
-    fn start_client(self) -> Result<Self::Client, Self::Error> {
-        let mut cfg = s2n_quic_tls_provider::rustls::client_config_builder()?
-            .with_root_certificates(self.root_store)
-            .with_client_auth_cert(self.my_cert_chain, self.my_private_key)?;
-
-        cfg.max_fragment_size = None;
-        cfg.alpn_protocols = vec![b"h3".to_vec()];
-        Ok(cfg.into())
-    }
-}
-
-impl MtlsProvider {
-    pub async fn new<A: AsRef<Path>, B: AsRef<Path>, C: AsRef<Path>>(
-        ca_cert_pem: A,
-        my_cert_pem: B,
-        my_key_pem: C,
-    ) -> Result<Self, RustlsError> {
-        let root_store = into_root_store(ca_cert_pem.as_ref()).await?;
-        let cert_chain = into_certificate(my_cert_pem.as_ref()).await?;
-        let private_key = into_private_key(my_key_pem.as_ref()).await?;
-        Ok(MtlsProvider {
-            root_store,
-            my_cert_chain: cert_chain.into_iter().map(CertificateDer::from).collect(),
-            my_private_key: private_key,
-        })
-    }
-}
-
 async fn read_file(path: &Path) -> Result<Vec<u8>, RustlsError> {
     let mut f = File::open(path)
         .await
@@ -105,7 +47,7 @@ async fn read_file(path: &Path) -> Result<Vec<u8>, RustlsError> {
     Ok(buf)
 }
 
-async fn into_certificate(path: &Path) -> Result<Vec<CertificateDer<'static>>, RustlsError> {
+pub async fn into_certificate(path: &Path) -> Result<Vec<CertificateDer<'static>>, RustlsError> {
     let buf = &read_file(path).await?;
     let mut cursor = Cursor::new(buf);
     rustls_pemfile::certs(&mut cursor)
@@ -115,7 +57,7 @@ async fn into_certificate(path: &Path) -> Result<Vec<CertificateDer<'static>>, R
         .collect()
 }
 
-async fn into_root_store(path: &Path) -> Result<RootCertStore, RustlsError> {
+pub async fn into_root_store(path: &Path) -> Result<RootCertStore, RustlsError> {
     let ca_certs: Vec<CertificateDer<'static>> = into_certificate(path)
         .await
         .map(|certs| certs.into_iter().map(CertificateDer::from))?
@@ -123,47 +65,4 @@ async fn into_root_store(path: &Path) -> Result<RootCertStore, RustlsError> {
     let mut cert_store = RootCertStore::empty();
     cert_store.add_parsable_certificates(ca_certs);
     Ok(cert_store)
-}
-
-async fn into_private_key(path: &Path) -> Result<PrivateKeyDer<'static>, RustlsError> {
-    let buf = &read_file(path).await?;
-    let mut cursor = Cursor::new(buf);
-
-    macro_rules! parse_key {
-        ($parser:ident, $key_type:expr) => {
-            cursor.set_position(0);
-
-            let keys: Result<Vec<_>, RustlsError> = rustls_pemfile::$parser(&mut cursor)
-                .map(|key| {
-                    key.map_err(|_| {
-                        RustlsError::General("Could not load any private keys".to_string())
-                    })
-                })
-                .collect();
-            match keys {
-                // try the next parser
-                Err(_) => (),
-                // try the next parser
-                Ok(keys) if keys.is_empty() => (),
-                Ok(mut keys) if keys.len() == 1 => {
-                    return Ok($key_type(keys.pop().unwrap()));
-                }
-                Ok(keys) => {
-                    return Err(RustlsError::General(format!(
-                        "Unexpected number of keys: {} (only 1 supported)",
-                        keys.len()
-                    )));
-                }
-            }
-        };
-    }
-
-    // attempt to parse PKCS8 encoded key. Returns early if a key is found
-    parse_key!(pkcs8_private_keys, PrivateKeyDer::Pkcs8);
-    // attempt to parse RSA key. Returns early if a key is found
-    parse_key!(rsa_private_keys, PrivateKeyDer::Pkcs1);
-
-    Err(RustlsError::General(
-        "could not load any valid private keys".to_string(),
-    ))
 }
