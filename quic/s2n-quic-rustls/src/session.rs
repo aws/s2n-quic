343fa0d3bb543120e8d6714974569755f9e5707e
Cameron Bytheway <bythewc@amazon.com>
2024-04-05 15:21:26 -0600

    Revert "feat(s2n-quic-rustls): update rustls from 0.21 to 0.23 (#2143)" (#2174)
    * Revert "feat(s2n-quic-rustls): update rustls from 0.21 to 0.23 (#2143)"
    
    This reverts commit 15f234c495f98cf61652cf70e8dbc3c7565098b4.
    
    * parse ec keys
    
    ---------
    
    Co-authored-by: Apoorv Kothari <apoorv@toidiu.com>

--- quic/s2n-quic-rustls/src/session.rs
+++ quic/s2n-quic-rustls/src/session.rs
@@ -6,7 +6,9 @@ use crate::cipher_suite::{
 };
 use bytes::Bytes;
 use core::{fmt, fmt::Debug, task::Poll};
-use rustls::quic::{self, Connection};
+use rustls::quic::{
+    Connection, {self},
+};
 use s2n_quic_core::{
     application::ServerName,
     crypto::{self, tls, tls::CipherSuite},
@@ -102,12 +104,9 @@ impl Session {
 
                 self.connection
                     .alert()
-                    .map(|alert| {
-                        // Explicitly annotate the type to detect if rustls starts
-                        // returning a large array
-                        let code: [u8; 1] = alert.to_array();
-                        let code = code[0];
-                        tls::Error { code, reason }
+                    .map(|alert| tls::Error {
+                        code: alert.get_u8(),
+                        reason,
                     })
                     .unwrap_or(tls::Error::INTERNAL_ERROR)
             })?;
