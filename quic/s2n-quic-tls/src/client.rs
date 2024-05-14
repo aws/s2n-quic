df5f5ea252e3326d16c9bb8802e49f01a27fcf50
apoorvko <apoorvko@amazon.com>
2024-05-13 20:51:38 -0700

    testing rustls fips?

--- quic/s2n-quic-tls/src/client.rs
+++ quic/s2n-quic-tls/src/client.rs
@@ -188,8 +188,6 @@ impl Builder {
     pub fn build(self) -> Result<Client, Error> {
         #[cfg(feature = "fips")]
         assert!(s2n_tls::init::fips_mode()?.is_enabled());
-        #[cfg(feature = "fips")]
-        panic!("test s2n-tls");
 
         Ok(Client {
             loader: self.config.build()?,
