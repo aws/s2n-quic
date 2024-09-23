43bfaaa6900f1cacb62f47cbeca353cee13a4b83
Mark Rousskov <thismark@amazon.com>
2024-09-23 16:08:06 +0000

    Add init bench for hmac keys

--- dc/s2n-quic-dc/benches/src/crypto.rs
+++ dc/s2n-quic-dc/benches/src/crypto.rs
@@ -5,8 +5,10 @@ use criterion::Criterion;
 
 pub mod encrypt;
 pub mod hkdf;
+pub mod hmac;
 
 pub fn benchmarks(c: &mut Criterion) {
     encrypt::benchmarks(c);
     hkdf::benchmarks(c);
+    hmac::benchmarks(c);
 }
