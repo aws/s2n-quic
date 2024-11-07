dc72f0c00b8525095194e83973b50957fdf08c16
camshaft <bytheway.cameron@gmail.com>
2024-11-07 23:47:19 +0000

    Deploying to gh-pages from @ aws/s2n-quic@799921c5994110d7ad45fbf4999e3236b0cfc0a0 🚀

--- toc.js
+++ toc.js
@@ -3,52 +3,68 @@
 // This is a script, and not included directly in the page, to control the total size of the book.
 // The TOC contains an entry for each page, so if each page includes a copy of the TOC,
 // the total size of the page becomes O(n**2).
-var sidebarScrollbox = document.querySelector("#sidebar .sidebar-scrollbox");
-sidebarScrollbox.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded affix "><li class="part-title">User Guide</li><li class="chapter-item expanded "><div><strong aria-hidden="true">2.</strong> Introduction</div></li><li class="chapter-item expanded "><a href="user-guide/installation.html"><strong aria-hidden="true">3.</strong> Installation</a></li><li class="chapter-item expanded affix "><li class="part-title">Developer Guide</li><li class="chapter-item expanded "><a href="dev-guide.html"><strong aria-hidden="true">4.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="dev-guide/setup.html"><strong aria-hidden="true">5.</strong> Setup</a></li><li class="chapter-item expanded "><a href="dev-guide/ci.html"><strong aria-hidden="true">6.</strong> Continuous Integration</a></li><li class="chapter-item expanded "><a href="dev-guide/kani.html"><strong aria-hidden="true">7.</strong> Kani</a></li><li class="chapter-item expanded affix "><li class="part-title">Examples</li><li class="chapter-item expanded "><a href="examples/async-client-hello-callback.html"><strong aria-hidden="true">8.</strong> Async client hello callback</a></li><li class="chapter-item expanded "><a href="examples/custom-congestion-controller.html"><strong aria-hidden="true">9.</strong> Custom congestion controller</a></li><li class="chapter-item expanded "><div><strong aria-hidden="true">10.</strong> dos mitigation</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">11.</strong> Echo</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">12.</strong> Event framework</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">13.</strong> Jumbo frame</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">14.</strong> rustls mtls</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">15.</strong> rustls provider</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">16.</strong> turmoil provider</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">17.</strong> Unreliable datagram</div></li></ol>';
-(function() {
-    let current_page = document.location.href.toString();
-    if (current_page.endsWith("/")) {
-        current_page += "index.html";
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
     }
-    var links = sidebarScrollbox.querySelectorAll("a");
-    var l = links.length;
-    for (var i = 0; i < l; ++i) {
-        var link = links[i];
-        var href = link.getAttribute("href");
-        if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
-            link.href = path_to_root + href;
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded affix "><li class="part-title">User Guide</li><li class="chapter-item expanded "><div><strong aria-hidden="true">2.</strong> Introduction</div></li><li class="chapter-item expanded "><a href="user-guide/installation.html"><strong aria-hidden="true">3.</strong> Installation</a></li><li class="chapter-item expanded affix "><li class="part-title">Developer Guide</li><li class="chapter-item expanded "><a href="dev-guide.html"><strong aria-hidden="true">4.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="dev-guide/setup.html"><strong aria-hidden="true">5.</strong> Setup</a></li><li class="chapter-item expanded "><a href="dev-guide/ci.html"><strong aria-hidden="true">6.</strong> Continuous Integration</a></li><li class="chapter-item expanded "><a href="dev-guide/kani.html"><strong aria-hidden="true">7.</strong> Kani</a></li><li class="chapter-item expanded affix "><li class="part-title">Examples</li><li class="chapter-item expanded "><a href="examples/async-client-hello-callback.html"><strong aria-hidden="true">8.</strong> Async client hello callback</a></li><li class="chapter-item expanded "><a href="examples/custom-congestion-controller.html"><strong aria-hidden="true">9.</strong> Custom congestion controller</a></li><li class="chapter-item expanded "><div><strong aria-hidden="true">10.</strong> dos mitigation</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">11.</strong> Echo</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">12.</strong> Event framework</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">13.</strong> Jumbo frame</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">14.</strong> rustls mtls</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">15.</strong> rustls provider</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">16.</strong> turmoil provider</div></li><li class="chapter-item expanded "><div><strong aria-hidden="true">17.</strong> Unreliable datagram</div></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString();
+        if (current_page.endsWith("/")) {
+            current_page += "index.html";
         }
-        // The "index" page is supposed to alias the first chapter in the book.
-        if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
-            link.classList.add("active");
-            var parent = link.parentElement;
-            while (parent) {
-                if (parent.tagName === "LI" && parent.previousElementSibling) {
-                    if (parent.previousElementSibling.classList.contains("chapter-item")) {
-                        parent.previousElementSibling.classList.add("expanded");
-                    }
+        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
+        var l = links.length;
+        for (var i = 0; i < l; ++i) {
+            var link = links[i];
+            var href = link.getAttribute("href");
+            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The "index" page is supposed to alias the first chapter in the book.
+            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
+                link.classList.add("active");
+                var parent = link.parentElement;
+                if (parent && parent.classList.contains("chapter-item")) {
+                    parent.classList.add("expanded");
                 }
-                parent = parent.parentElement;
+                while (parent) {
+                    if (parent.tagName === "LI" && parent.previousElementSibling) {
+                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
+                            parent.previousElementSibling.classList.add("expanded");
+                        }
+                    }
+                    parent = parent.parentElement;
+                }
             }
         }
-    }
-})();
-
-// Track and set sidebar scroll position
-sidebarScrollbox.addEventListener('click', function(e) {
-    if (e.target.tagName === 'A') {
-        sessionStorage.setItem('sidebar-scroll', sidebarScrollbox.scrollTop);
-    }
-}, { passive: true });
-var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
-sessionStorage.removeItem('sidebar-scroll');
-if (sidebarScrollTop) {
-    // preserve sidebar scroll position when navigating via links within sidebar
-    sidebarScrollbox.scrollTop = sidebarScrollTop;
-} else {
-    // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
-    var activeSection = document.querySelector('#sidebar .active');
-    if (activeSection) {
-        activeSection.scrollIntoView({ block: 'center' });
+        // Track and set sidebar scroll position
+        this.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
+            var activeSection = document.querySelector('#sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(function (el) {
+            el.addEventListener('click', toggleSection);
+        });
     }
 }
+window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
