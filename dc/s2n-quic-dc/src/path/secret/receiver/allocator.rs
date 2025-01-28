895b7d2719094b942e8eff6995382ee58cda64ef
Mark Rousskov <thismark@amazon.com>
2025-01-28 16:17:40 +0000

    Add license headers

--- dc/s2n-quic-dc/src/path/secret/receiver/allocator.rs
+++ dc/s2n-quic-dc/src/path/secret/receiver/allocator.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 //! Slab allocator drawing from a fixed arena.
 //!
 //! The arena is allocated at initialization time, providing a fixed memory region from which to
