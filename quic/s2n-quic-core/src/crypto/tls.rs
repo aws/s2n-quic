c8f04b59c94c3052ff00ff102b450eb196ac7393
taikulawo <qaq1362211689@gmail.com>
2025-03-25 11:47:52 +0800

    update

--- quic/s2n-quic-core/src/crypto/tls.rs
+++ quic/s2n-quic-core/src/crypto/tls.rs
@@ -16,6 +16,8 @@ pub mod testing;
 
 #[cfg(all(feature = "alloc", any(test, feature = "testing")))]
 pub mod null;
+#[cfg(all(feature = "alloc", any(test, feature = "testing")))]
+pub mod null_tls_context;
 
 /// Holds all application parameters which are exchanged within the TLS handshake.
 #[derive(Debug)]
--- quic/s2n-quic/src/tests/tls_context.rs
+++ quic/s2n-quic/src/tests/tls_context.rs
@@ -5,17 +5,23 @@ use super::*;
 use crate::{provider::tls::Provider, stream::PeerStream};
 
 use crate::provider::io::testing::{spawn, Result};
-use s2n_quic_core::crypto::tls::{null, null::server::UserProvidedTlsContext};
+use s2n_quic_core::crypto::tls::null_tls_context::{self, UserProvidedTlsContext};
 
-#[derive(Default)]
-pub struct NoTlsProvider {}
+pub struct NoTlsProvider {
+    ctx: UserProvidedTlsContext,
+}
+impl NoTlsProvider {
+    pub fn new(ctx: UserProvidedTlsContext) -> Self {
+        Self { ctx }
+    }
+}
 impl Provider for NoTlsProvider {
-    type Server = null::Endpoint;
-    type Client = null::Endpoint;
+    type Server = null_tls_context::Endpoint;
+    type Client = null_tls_context::Endpoint;
     type Error = String;
 
     fn start_server(self) -> Result<Self::Server, Self::Error> {
-        Ok(Self::Server::default())
+        Ok(null_tls_context::Endpoint(self.ctx.clone()))
     }
 
     fn start_client(self) -> Result<Self::Client, Self::Error> {
@@ -26,16 +32,17 @@ impl Provider for NoTlsProvider {
 #[test]
 fn no_tls_test() {
     let model = Model::default();
+    let ctx = UserProvidedTlsContext { conf: "foo".into() };
     test(model, |handle| {
         let server = Server::builder()
             .with_io(handle.builder().build()?)?
-            .with_tls(NoTlsProvider::default())?
+            .with_tls(NoTlsProvider::new(ctx.clone()))?
             .with_event(tracing_events())?
             .with_random(Random::with_seed(456))?
             .start()?;
         let client = Client::builder()
             .with_io(handle.builder().build()?)?
-            .with_tls(NoTlsProvider::default())?
+            .with_tls(NoTlsProvider::new(ctx))?
             .with_event(tracing_events())?
             .with_random(Random::with_seed(456))?
             .start()?;
@@ -56,7 +63,7 @@ pub fn start_server(mut server: Server) -> Result<SocketAddr> {
             let ctx: Box<UserProvidedTlsContext> =
                 connection.take_tls_context().unwrap().downcast().unwrap();
             // continue our logic
-            let _ = ctx.conf;
+            assert_eq!("foo", ctx.conf);
             tracing::debug!("accepted server connection: {}", connection.id());
             spawn(async move {
                 while let Ok(Some(stream)) = connection.accept().await {
