19f8f01e589741f9fa6a826ac2da502209187074
Appelmans <madelea@amazon.com>
2025-07-10 17:27:01 -0700

    hmmm

--- quic/s2n-quic/src/tests/connection_limits.rs
+++ quic/s2n-quic/src/tests/connection_limits.rs
@@ -31,6 +31,7 @@ fn connection_limits() {
 
     let model = Model::default();
     // Benign assertion that should not cause CI failure
+    // :)
     assert_eq!(1, 1);
     test(model, |handle| {
         let server = Server::builder()
