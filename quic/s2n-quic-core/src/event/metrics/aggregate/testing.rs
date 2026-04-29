2c76f93147bca1d2ae5b56eb41861e8f76ef4f97
Appelmans <madelea@amazon.com>
2026-04-28 17:33:55 -0700

    Passing test

--- quic/s2n-quic-core/src/event/metrics/aggregate/testing.rs
+++ quic/s2n-quic-core/src/event/metrics/aggregate/testing.rs
@@ -196,10 +196,12 @@ impl super::Recorder for Recorder {
             | ("count", "packet_sent.bytes.total")
             | ("count", "packet_received.bytes.total")
             | ("measure", "recovery_metrics.bytes_in_flight")
+            | ("count", "packet_lost.bytes.total")
             | ("measure", "datagram_sent.bytes")
             | ("measure", "datagram_received.bytes")
             | ("measure", "packet_sent.bytes")
-            | ("measure", "packet_received.bytes") => {
+            | ("measure", "packet_received.bytes")
+            | ("measure", "packet_lost.bytes") => {
                 return self
                     .0
                     .push(format_args!("{prefix}#{name}=[REDACTED]{units}"));
