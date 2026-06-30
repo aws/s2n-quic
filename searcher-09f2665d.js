328dfa5ba3678501df6050a160200cea52a6a2af
Mark-Simulacrum <mark.simulacrum@gmail.com>
2026-06-30 17:19:08 +0000

    Deploying to gh-pages from @ aws/s2n-quic@058014cd76e2828ac55d1602f227e08e9da6a82f 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2d692c52.js',
+                path_to_root + 'searchindex-1de4198e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
