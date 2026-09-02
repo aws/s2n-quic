2be9db587674f678fac588a1e65739ea86ac0344
Mark Rousskov <thismark@amazon.com>
2026-09-02 19:05:29 -0400

    test: cover large Client/ServerHello packets (#3299)

--- quic/s2n-quic-tests/src/tests.rs
+++ quic/s2n-quic-tests/src/tests.rs
@@ -73,6 +73,8 @@ mod fips;
 #[cfg(s2n_tls_provider)]
 mod mtls;
 #[cfg(s2n_tls_provider)]
+mod pq_initial_mtu;
+#[cfg(s2n_tls_provider)]
 mod signature_scheme;
 // This test uses real OS sockets, which conflicts with bach's simulated time scope on Windows.
 #[cfg(not(target_os = "windows"))]
